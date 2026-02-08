aa20631a36fc66b1b524fa3ef3175424baf59282
liia <kirilich_lubit@rambler.ru>
2026-02-09 04:04:51 +0500

    credentials test

--- nabieva.liia/S0/test-credentials.cpp
+++ nabieva.liia/S0/test-credentials.cpp
@@ -1,9 +1,9 @@
-#define BOOST_TEST_MODULE Test credentials
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
+#include <iostream>
 
 BOOST_AUTO_TEST_CASE(credentials_test)
 {
-	int i = 1;
-	BOOST_TEST(i);
-	BOOST_TEST(i == 2);
+	std::ostringstream out;
+	nabieva::out_credentials(out);
+	BOOST_TEST(out.str() = "nabieva.liia");
 }
