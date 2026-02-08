a1ef09cd8a7aad8a5b0f6cacd22c216e435cd0fb
ArtConion <artiom0408@mail.ru>
2026-02-09 02:16:37 +0300

    fix unit

--- vishnyakov.artiom/S0/test-credentials.cpp
+++ vishnyakov.artiom/S0/test-credentials.cpp
@@ -5,7 +5,7 @@
 BOOST_AUTO_TEST_CASE(credentials_test)
 {
   std::ostringstream out;
-  karpenko::out_credentials(out);
+  vishnaykov::out_credentials(out);
   BOOST_TEST(out.str() == "vishnyakov.artiom");
 }
 
