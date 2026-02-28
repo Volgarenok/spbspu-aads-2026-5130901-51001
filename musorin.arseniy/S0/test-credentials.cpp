672c3f9ebe58fa6bc8efd857262784dcf4a59f06
arseniymusorin@gmail.com <quaka@quaka.localdomain>
2026-02-28 18:06:35 +0300

    credentials test

---
+++ musorin.arseniy/S0/test-credentials.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE Test credentials
+#include <boost/test/included/unit_test.hpp>
+#include "credentials.hpp"
+#include <sstream>
+BOOST_AUTO_TEST_CASE(credential_test)
+{
+  std::ostringstream out;
+  musorin::out_credentials(out);
+  BOOST_TEST(out.str() == "musorin.arseniy"); 
+}
