adf63a227a4cd44503cf6998bf62780bb173c624
platon <platonalekseevi6@gmail.com>
2026-03-01 07:59:30 +0300

    feat(list): add begin/end/before_begin + empty/front

--- alekseev.platon/S1/list.hpp
+++ alekseev.platon/S1/list.hpp
@@ -46,6 +46,72 @@ namespace alekseev
   private:
     detail::NodeBase sentinel_;
   };
+
+  template< class T >
+  List< T >::List()
+  {
+    sentinel_.next = nullptr;
+  }
+
+  template< class T >
+  List< T >::~List()
+  {
+    clear();
+  }
+
+  template< class T >
+  typename List< T >::iterator List< T >::before_begin()
+  {
+    return iterator(&sentinel_);
+  }
+
+  template< class T >
+  typename List< T >::const_iterator List< T >::cbefore_begin() const
+  {
+    return const_iterator(&sentinel_);
+  }
+
+  template< class T >
+  typename List< T >::iterator List< T >::begin()
+  {
+    return iterator(sentinel_.next);
+  }
+
+  template< class T >
+  typename List< T >::iterator List< T >::end()
+  {
+    return iterator(nullptr);
+  }
+
+  template< class T >
+  typename List< T >::const_iterator List< T >::cbegin() const
+  {
+    return const_iterator(sentinel_.next);
+  }
+
+  template< class T >
+  typename List< T >::const_iterator List< T >::cend() const
+  {
+    return const_iterator(nullptr);
+  }
+
+  template< class T >
+  bool List< T >::empty() const
+  {
+    return sentinel_.next == nullptr;
+  }
+
+  template< class T >
+  T & List< T >::front()
+  {
+    return static_cast< detail::Node< T > * >(sentinel_.next)->data;
+  }
+
+  template< class T >
+  const T & List< T >::front() const
+  {
+    return static_cast< const detail::Node< T > * >(sentinel_.next)->data;
+  }
 }
 
 #endif
