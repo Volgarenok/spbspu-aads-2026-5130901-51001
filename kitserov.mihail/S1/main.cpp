d341770b2f87faccccf20cfb8c8bd97decdb1437
Kreniker <kitserov.mike@gmail.com>
2026-03-03 18:48:38 +0300

    start classes

--- kitserov.mihail/S1/main.cpp
+++ kitserov.mihail/S1/main.cpp
@@ -1,2 +1,26 @@
+namespace kitserov
+{
+  template< class T > class List;
+  template < class T >
+  class LIter
+  {
+    friend class List< T >;
+    
+  };
+
+  template< class T >
+  class LCIter
+  {
+    friend class List< T >;
+    
+  };
+
+  template< class T >
+  class List
+  {
+    
+  };
+}
+
 int main()
 {}
