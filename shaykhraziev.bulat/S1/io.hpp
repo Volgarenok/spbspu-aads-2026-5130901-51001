c19a3e22fe52082a0d32435e660456c4e83fad7a
CWSHBR <mrcookie209@gmail.com>
2026-03-10 00:33:23 +0300

    refactor: move templates to .hpp, drop .cpp include trick list.cpp/io.cpp/app.cpp removed. All template and inline code now lives in list.hpp/io.hpp/app.hpp with proper include guards. main.cpp is the single translation unit. Fixes Clangd redefinition errors.

--- shaykhraziev.bulat/S1/io.hpp
+++ shaykhraziev.bulat/S1/io.hpp
@@ -1,7 +1,43 @@
 #ifndef IO_HPP
 #define IO_HPP
 
-#include "io.cpp"
+#include <iostream>
+
+namespace shaykhraziev
+{
+  namespace detail
+  {
+    inline void skipSpaces(std::istream& in)
+    {
+      while (in.peek() == ' ' || in.peek() == '\t')
+      {
+        in.get();
+      }
+    }
+
+    inline bool isLineEnd(std::istream& in)
+    {
+      int c = in.peek();
+      return c == '\n' || c == '\r' || c == std::istream::traits_type::eof();
+    }
+
+    inline void skipLine(std::istream& in)
+    {
+      while (!isLineEnd(in))
+      {
+        in.get();
+      }
+      if (in.peek() == '\r')
+      {
+        in.get();
+      }
+      if (in.peek() == '\n')
+      {
+        in.get();
+      }
+    }
+  }
+}
 
 #endif
 
