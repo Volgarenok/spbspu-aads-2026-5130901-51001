b107db051421b778507e115183315c115a615bd9
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-03-10 17:21:59 +0300

    add destructots/assignment operators

--- krivoshapov.ivan/S1/list.hpp
+++ krivoshapov.ivan/S1/list.hpp
@@ -36,6 +36,166 @@ namespace krivoshapov
         throw;
       }
     }
+    List(List &&other) noexcept : head_(other.head_),
+                                  tail_(other.tail_),
+                                  size_(other.size_)
+    {
+      other.head_ = nullptr;
+      other.tail_ = nullptr;
+      other.size_ = 0;
+    }
+
+    ~List()
+    {
+      clear();
+    }
+
+    List &operator=(const List &other)
+    {
+      if (this != &other)
+      {
+        List tmp(other);
+        swapWith(tmp);
+      }
+      return *this;
+    }
+
+    List &operator=(List &&other) noexcept
+    {
+      if (this != &other)
+      {
+        clear();
+        head_ = other.head_;
+        tail_ = other.tail_;
+        size_ = other.size_;
+        other.head_ = nullptr;
+        other.tail_ = nullptr;
+        other.size_ = 0;
+      }
+      return *this;
+    }
+    List(List &&other) noexcept : head_(other.head_),
+                                  tail_(other.tail_),
+                                  size_(other.size_)
+    {
+      other.head_ = nullptr;
+      other.tail_ = nullptr;
+      other.size_ = 0;
+    }
+
+    ~List()
+    {
+      clear();
+    }
+
+    List &operator=(const List &other)
+    {
+      if (this != &other)
+      {
+        List tmp(other);
+        swapWith(tmp);
+      }
+      return *this;
+    }
+
+    List &operator=(List &&other) noexcept
+    {
+      if (this != &other)
+      {
+        clear();
+        head_ = other.head_;
+        tail_ = other.tail_;
+        size_ = other.size_;
+        other.head_ = nullptr;
+        other.tail_ = nullptr;
+        other.size_ = 0;
+      }
+      return *this;
+    }
+
+    iterator begin()
+    {
+      return iterator(head_);
+    }
+
+    iterator end()
+    {
+      return iterator(nullptr);
+    }
+
+    const_iterator begin() const
+    {
+      return const_iterator(head_);
+    }
+
+    const_iterator end() const
+    {
+      return const_iterator(nullptr);
+    }
+
+    const_iterator cbegin() const
+    {
+      return const_iterator(head_);
+    }
+
+    const_iterator cend() const
+    {
+      return const_iterator(nullptr);
+    }
+
+    T &front()
+    {
+      return head_->value_;
+    }
+
+    const T &front() const
+    {
+      return head_->value_;
+    }
+
+    T &back()
+    {
+      return tail_->value_;
+    }
+
+    const T &back() const
+    {
+      return tail_->value_;
+    }
+
+    bool empty() const
+    {
+      return size_ == 0;
+    }
+
+    int size() const
+    {
+      return size_;
+    }
+
+    void pushFront(const T &value)
+    {
+      Node *node = new Node(value);
+      linkFront(node);
+    }
+
+    void pushFront(T &&value)
+    {
+      Node *node = new Node(static_cast<T &&>(value));
+      linkFront(node);
+    }
+
+    void pushBack(const T &value)
+    {
+      Node *node = new Node(value);
+      linkBack(node);
+    }
+
+    void pushBack(T &&value)
+    {
+      Node *node = new Node(static_cast<T &&>(value));
+      linkBack(node);
+    }
   };
 }
 #endif
