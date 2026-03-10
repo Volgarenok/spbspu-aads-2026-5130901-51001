5739224bfced69abc4944788253f3d42b643eb19
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-03-10 17:35:52 +0300

    add readInput realization

--- krivoshapov.ivan/S1/main.cpp
+++ krivoshapov.ivan/S1/main.cpp
@@ -1,4 +1,35 @@
 #include <iostream>
+#include <limits>
+#include <string>
+
+#include "list.hpp"
+#include "named-seq.hpp"
+namespace krivoshapov
+{
+  static void readInput(List<NamedSeq> &seqs)
+  {
+    std::string name;
+    while (std::cin >> name)
+    {
+      NamedSeq seq(name);
+      while (std::cin.peek() != '\n' && !std::cin.eof())
+      {
+        int n = 0;
+        if (!(std::cin >> n))
+        {
+          std::cin.clear();
+          break;
+        }
+        seq.nums.pushBack(n);
+      }
+      if (!std::cin.eof())
+      {
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      }
+      seqs.pushBack(static_cast<NamedSeq &&>(seq));
+    }
+  }
+}
 
 int main()
 {
