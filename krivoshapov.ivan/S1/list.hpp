0ecf6d492011cb2abdc1a4379a3d0b3ec2e0fd64
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-03-10 17:17:03 +0300

    add default/try-catch

--- krivoshapov.ivan/S1/list.hpp
+++ krivoshapov.ivan/S1/list.hpp
@@ -6,5 +6,36 @@
 
 namespace krivoshapov
 {
+  template <class T>
+  class List
+  {
+  public:
+    using iterator = LIter<T>;
+    using const_iterator = LCIter<T>;
+
+    List() : head_(nullptr),
+             tail_(nullptr),
+             size_(0)
+    {
+    }
+
+    List(const List &other) : head_(nullptr),
+                              tail_(nullptr),
+                              size_(0)
+    {
+      try
+      {
+        for (auto it = other.cbegin(); it != other.cend(); ++it)
+        {
+          pushBack(*it);
+        }
+      }
+      catch (...)
+      {
+        clear();
+        throw;
+      }
+    }
+  };
 }
 #endif
