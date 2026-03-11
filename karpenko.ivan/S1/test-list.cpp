7420ea4a7b8638bc9dd619d99f16951bd0d2054c
restorte <vanyakarpenko837@yandex.ru>
2026-03-12 02:11:15 +0300

    fix CG

--- karpenko.ivan/S1/test-list.cpp
+++ karpenko.ivan/S1/test-list.cpp
@@ -21,7 +21,7 @@ BOOST_AUTO_TEST_CASE(test_push_front)
   lst.push_front(42);
   BOOST_CHECK(!lst.empty());
   BOOST_CHECK(lst.front() == 42);
-  
+
   lst.push_front(24);
   BOOST_CHECK(lst.front() == 24);
 }
@@ -86,7 +86,7 @@ BOOST_AUTO_TEST_CASE(test_iterator)
   lst.push_back(10);
   lst.push_back(20);
   lst.push_back(30);
-  
+
   List< int >::iterator it = lst.begin();
   BOOST_CHECK(*it == 10);
   ++it;
@@ -101,7 +101,7 @@ BOOST_AUTO_TEST_CASE(test_const_iterator)
 {
   List< int > lst;
   lst.push_back(42);
-  
+
   List< int >::const_iterator it = lst.cbegin();
   BOOST_CHECK(*it == 42);
   ++it;
@@ -113,7 +113,7 @@ BOOST_AUTO_TEST_CASE(test_copy_constructor)
   List< int > a;
   a.push_back(1);
   a.push_back(2);
-  
+
   List< int > b(a);
   BOOST_CHECK(b.size() == 2);
   BOOST_CHECK(b.front() == 1);
@@ -124,7 +124,7 @@ BOOST_AUTO_TEST_CASE(test_move_constructor)
 {
   List< int > a;
   a.push_back(99);
-  
+
   List< int > b(std::move(a));
   BOOST_CHECK(a.empty());
   BOOST_CHECK(b.size() == 1);
@@ -136,7 +136,7 @@ BOOST_AUTO_TEST_CASE(test_assignment)
   List< int > a;
   a.push_back(5);
   a.push_back(6);
-  
+
   List< int > b;
   b = a;
   BOOST_CHECK(b.size() == 2);
@@ -149,10 +149,10 @@ BOOST_AUTO_TEST_CASE(test_swap)
   List< int > a;
   a.push_back(1);
   a.push_back(2);
-  
+
   List< int > b;
   b.push_back(3);
-  
+
   a.swap(b);
   BOOST_CHECK(a.size() == 1);
   BOOST_CHECK(a.front() == 3);
