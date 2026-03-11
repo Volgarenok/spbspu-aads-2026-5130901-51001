355ef4c5da39f534bb3a3f27add70c3c4f610aab
restorte <vanyakarpenko837@yandex.ru>
2026-03-11 23:50:07 +0300

    add basic structures of nodes for the cyclic list

--- karpenko.ivan/S1/main.cpp
+++ karpenko.ivan/S1/main.cpp
@@ -1,3 +1,26 @@
+#include <cstddef>
+
+namespace karpenko
+{
+  namespace detail
+  {
+    struct NodeBase
+    {
+      NodeBase* next;
+      explicit NodeBase(NodeBase* nxt = nullptr) noexcept : next(nxt) {}
+    };
+
+    template< typename T >
+    struct Node final : NodeBase
+    {
+      T data;
+
+      explicit Node(NodeBase* nxt, const T& value) : NodeBase(nxt), data(value) {}
+      explicit Node(NodeBase* nxt, T&& value) : NodeBase(nxt), data(std::move(value)) {}
+    };
+  }
+}
+
 int main()
 {
 
