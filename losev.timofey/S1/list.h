ba38593761fc1286cb2f72d744d7a8952c08b788
L0syash <losev_timofey07@mail.ru>
2026-03-13 20:35:34 +0300

    Implement pop_front

--- losev.timofey/S1/list.h
+++ losev.timofey/S1/list.h
@@ -20,14 +20,23 @@ public:
   List() : head_(nullptr) {}
   
   bool empty() const { return head_ == nullptr; }
-  
+
   void push_front(const T& value) {
     head_ = new Node<T>(value, head_);
   }
-  
+
+  void pop_front() {
+    if (head_ == nullptr) return;
+    Node<T>* temp = head_;
+    head_ = head_->next;
+    delete temp;
+  }
+
 private:
   Node<T>* head_;
 };
+
+
 
 }
 
