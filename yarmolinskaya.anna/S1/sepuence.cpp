a15f67cf69064bfd893ee0b0c603f270aa00445c
prblmtc11 <prblmtc11@gmail.com>
2026-03-14 02:07:41 +0300

    added sequence.cpp

---
+++ yarmolinskaya.anna/S1/sepuence.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "sequence.hpp"
+
+namespace yarmolinskaya
+{
+
+void readSequences(List<NamedSequence>& data)
+{
+  std::string name;
+
+  while (std::cin >> name)
+  {
+    List<int> nums;
+    int value;
+
+    while (std::cin >> value)
+    {
+      nums.push_back(value);
+    }
+
+    std::cin.clear();
+    data.push_back(NamedSequence{name, nums});
+  }
+}
+
+void printNames(const List<NamedSequence>& data)
+{
+  bool first = true;
+
+  for (auto it = data.cbegin(); it != data.cend(); ++it)
+  {
+    if (!first)
+    {
+      std::cout << " ";
+    }
+
+    std::cout << (*it).first;
+    first = false;
+  }
+
+  std::cout << "\n";
+}
+
+}
