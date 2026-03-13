472074dc70939c900da9ba3fa16bd87b5311fa0d
L0syash <losev_timofey07@mail.ru>
2026-03-13 21:37:58 +0300

    Fix order

--- losev.timofey/S1/list.cpp
+++ losev.timofey/S1/list.cpp
@@ -2,125 +2,107 @@
 
 namespace losev {
 
-template< class T >
-Node< T >::Node(const T& value, Node* n) : data(value), next(n) {}
+// Node
+template<class T>
+Node<T>::Node(const T& value, Node* n) : data(value), next(n) {}
 
-template< class T >
-LIter< T >::LIter(Node< T >* ptr) : ptr_(ptr) {}
+// LIter
+template<class T>
+LIter<T>::LIter(Node<T>* ptr) : ptr_(ptr) {}
 
-template< class T >
-bool LIter< T >::operator==(const LIter& other) const {
+template<class T>
+bool LIter<T>::operator==(const LIter& other) const {
   return ptr_ == other.ptr_;
 }
 
-template< class T >
-bool LIter< T >::operator!=(const LIter& other) const {
+template<class T>
+bool LIter<T>::operator!=(const LIter& other) const {
   return ptr_ != other.ptr_;
 }
 
-template< class T >
-T& LIter< T >::operator*() {
+template<class T>
+T& LIter<T>::operator*() {
   return ptr_->data;
 }
 
-template< class T >
-const T& LIter< T >::operator*() const {
+template<class T>
+const T& LIter<T>::operator*() const {
   return ptr_->data;
 }
 
-template< class T >
-LIter< T >& LIter< T >::operator++() {
+template<class T>
+LIter<T>& LIter<T>::operator++() {
   ptr_ = ptr_->next;
   return *this;
 }
 
-template< class T >
-LIter< T > LIter< T >::operator++(int) {
-  LIter< T > temp = *this;
+template<class T>
+LIter<T> LIter<T>::operator++(int) {
+  LIter<T> temp = *this;
   ptr_ = ptr_->next;
   return temp;
 }
 
-template< class T >
-LCIter< T >::LCIter(const Node< T >* ptr) : ptr_(ptr) {}
+template<class T>
+T* LIter<T>::operator->() { 
+  return &(ptr_->data); 
+}
 
-template< class T >
-const T& LCIter< T >::operator*() const {
+template<class T>
+const T* LIter<T>::operator->() const { 
+  return &(ptr_->data); 
+}
+
+// LCIter
+template<class T>
+LCIter<T>::LCIter(const Node<T>* ptr) : ptr_(ptr) {}
+
+template<class T>
+const T& LCIter<T>::operator*() const {
   return ptr_->data;
 }
 
-template< class T >
-const T* LCIter< T >::operator->() const {
+template<class T>
+const T* LCIter<T>::operator->() const {
   return &(ptr_->data);
 }
 
-template< class T >
-LCIter< T >& LCIter< T >::operator++() {
+template<class T>
+LCIter<T>& LCIter<T>::operator++() {
   ptr_ = ptr_->next;
   return *this;
 }
 
-template< class T >
-LCIter< T > LCIter< T >::operator++(int) {
-  LCIter< T > temp = *this;
+template<class T>
+LCIter<T> LCIter<T>::operator++(int) {
+  LCIter<T> temp = *this;
   ptr_ = ptr_->next;
   return temp;
 }
 
-template< class T >
-bool LCIter< T >::operator==(const LCIter& other) const {
+template<class T>
+bool LCIter<T>::operator==(const LCIter& other) const {
   return ptr_ == other.ptr_;
 }
 
-template< class T >
-bool LCIter< T >::operator!=(const LCIter& other) const {
+template<class T>
+bool LCIter<T>::operator!=(const LCIter& other) const {
   return ptr_ != other.ptr_;
 }
 
-template< class T >
-List< T >::List(const List& other) : head_(nullptr) {
+// List
+template<class T>
+List<T>::List(const List& other) : head_(nullptr) {
   copy_from(other);
 }
 
-template< class T >
-void List< T >::copy_from(const List& other) {
-  head_ = nullptr;
-  if (other.head_ == nullptr) {
-    return;
-  }
-  head_ = new Node< T >(other.head_->data, nullptr);
-  Node< T >* current_new = head_;
-  Node< T >* current_other = other.head_->next;
-  
-  while (current_other != nullptr) {
-    current_new->next = new Node< T >(current_other->data, nullptr);
-    current_new = current_new->next;
-    current_other = current_other->next;
-  }
-}
-
-template< class T >
-List< T >::~List() {
+template<class T>
+List<T>::~List() {
   clear();
 }
 
-template< class T >
-void List< T >::clear() {
-  while (head_ != nullptr) {
-    Node< T >* temp = head_;
-    head_ = head_->next;
-    delete temp;
-  }
-}
-
-template< class T >
-T* LIter< T >::operator->() { return &(ptr_->data); }
-
-template< class T >
-const T* LIter< T >::operator->() const { return &(ptr_->data); }
-
-template< class T >
-List< T >& List< T >::operator=(const List& other) {
+template<class T>
+List<T>& List<T>::operator=(const List& other) {
   if (this != &other) {
     clear();
     copy_from(other);
@@ -128,12 +110,38 @@ List< T >& List< T >::operator=(const List& other) {
   return *this;
 }
 
-template struct Node< int >;
-template struct Node< NamedList >;   
-template class List< int >;
-template class List< NamedList >; 
-template class LIter< int >;
-template class LIter< NamedList >; 
-template class LCIter< int >;
-template class LCIter< NamedList >; 
+template<class T>
+void List<T>::copy_from(const List& other) {
+  head_ = nullptr;
+  if (other.head_ == nullptr) {
+    return;
+  }
+  head_ = new Node<T>(other.head_->data, nullptr);
+  Node<T>* current_new = head_;
+  Node<T>* current_other = other.head_->next;
+  
+  while (current_other != nullptr) {
+    current_new->next = new Node<T>(current_other->data, nullptr);
+    current_new = current_new->next;
+    current_other = current_other->next;
+  }
+}
+
+template<class T>
+void List<T>::clear() {
+  while (head_ != nullptr) {
+    Node<T>* temp = head_;
+    head_ = head_->next;
+    delete temp;
+  }
+}
+
+template struct Node<int>;
+template struct Node<NamedList>;
+template class LIter<int>;
+template class LIter<NamedList>;
+template class LCIter<int>;
+template class LCIter<NamedList>;
+template class List<int>;
+template class List<NamedList>;
 }
--- losev.timofey/S1/list.h
+++ losev.timofey/S1/list.h
@@ -6,35 +6,23 @@
 
 namespace losev {
 
-template< class T >
+template<class T> class List;
+template<class T> class LIter;
+template<class T> class LCIter;
+
+template<class T>
 struct Node {
   T data;
   Node* next;
   Node(const T& value, Node* n = nullptr);
 };
 
-template< class T >
-class LCIter {
-  friend class List< T >;
-private:
-  const Node< T >* ptr_;
-  explicit LCIter(const Node< T >* ptr = nullptr);
-public:
-  const T& operator*() const;
-  const T* operator->() const;
-  LCIter& operator++();
-  LCIter operator++(int);
-  bool operator==(const LCIter& other) const;
-  bool operator!=(const LCIter& other) const;
-  const T* operator->() const;
-};
-
-template< class T >
+template<class T>
 class LIter {
-  friend class List< T >;
+  friend class List<T>;
 private:
-  Node< T >* ptr_;
-  explicit LIter(Node< T >* ptr = nullptr);
+  Node<T>* ptr_;
+  explicit LIter(Node<T>* ptr = nullptr);
 public:
   bool operator==(const LIter& other) const;
   bool operator!=(const LIter& other) const;
@@ -46,49 +34,60 @@ public:
   const T* operator->() const;
 };
 
-template< class T >
+template<class T>
+class LCIter {
+  friend class List<T>;
+private:
+  const Node<T>* ptr_;
+  explicit LCIter(const Node<T>* ptr = nullptr);
+public:
+  const T& operator*() const;
+  const T* operator->() const;
+  LCIter& operator++();
+  LCIter operator++(int);
+  bool operator==(const LCIter& other) const;
+  bool operator!=(const LCIter& other) const;
+};
+
+template<class T>
 class List {
 public:
-  using iterator = LIter< T >;
+  using iterator = LIter<T>;
   iterator begin() { return iterator(head_); }
   iterator end() { return iterator(nullptr); }
 
-  using const_iterator = LCIter< T >;
+  using const_iterator = LCIter<T>;
   const_iterator begin() const { return const_iterator(head_); }
   const_iterator end() const { return const_iterator(nullptr); }
 
   List() : head_(nullptr) {}
-
   List(const List& other);
-
   ~List();
 
   bool empty() const { return head_ == nullptr; }
 
   void push_front(const T& value) {
-    head_ = new Node< T >(value, head_);
+    head_ = new Node<T>(value, head_);
   }
 
   void pop_front() {
     if (head_ == nullptr) return;
-    Node< T >* temp = head_;
+    Node<T>* temp = head_;
     head_ = head_->next;
     delete temp;
   }
 
   List& operator=(const List& other);
-
   void clear();
 
 private:
-  Node< T >* head_;
-
+  Node<T>* head_;
   void copy_from(const List& other);
 };
 
 struct NamedList {
   std::string name;
-  List< int > numbers;
+  List<int> numbers;
 };
 
 }
