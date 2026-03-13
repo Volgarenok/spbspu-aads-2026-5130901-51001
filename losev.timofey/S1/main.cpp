31d1dcad4ff31ed0a63ebeb8680146db805f0f2d
L0syash <losev_timofey07@mail.ru>
2026-03-13 22:55:06 +0300

    wtf code 2

--- losev.timofey/S1/main.cpp
+++ losev.timofey/S1/main.cpp
@@ -1,8 +1,5 @@
 #include <iostream>
 #include <string>
-#include <climits>
-#include <cerrno>
-#include <cstdlib>
 #include "list.h"
 
 namespace losev {
@@ -34,19 +31,6 @@ T get_element_at(const List< T >& list, size_t index) {
   return *it;
 }
 
-bool read_number(const std::string& token, unsigned long long& result) {
-  if (token.empty()) return false;
-
-  char* endptr;
-  errno = 0;
-  result = strtoull(token.c_str(), &endptr, 10);
-
-  if (errno == ERANGE || *endptr != '\0') {
-    return false;
-  }
-  return true;
-}
-
 }
 int main() {
   using namespace losev;
@@ -65,12 +49,8 @@ int main() {
         std::string token;
         std::cin >> token;
         unsigned long long big_num;
-        if (!read_number(token, big_num)) {
-          std::cerr << "overflow" << std::endl;
-          return 1;
-        }
         std::cerr << "overflow" << "\n";
-        std::exit(1);
+        return 1;
       }
       seq.numbers.push_front(num);
     }
