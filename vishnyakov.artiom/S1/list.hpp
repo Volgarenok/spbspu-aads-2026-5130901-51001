da6fb823aaec23354dee4de5e4128f3303396fc2
ArtConion <artiom0408@mail.ru>
2026-03-13 21:38:12 +0300

    add LCIter class

--- vishnyakov.artiom/S1/list.hpp
+++ vishnyakov.artiom/S1/list.hpp
@@ -57,13 +57,65 @@ namespace vishnyakov
   class LCIter
   {
     friend class List< T >;
+    Node* node_;
+
+  public:
+    LIter(const Node* node = nullptr):
+      node_(node)
+    {}
+
+    LIter(const LIter< T > it):
+      node_(it.node_)
+    {}
+
+    T* operator*() const
+    {
+      return node_->data_;
+    }
+
+    T& operator->() const
+    {
+      return *node_->data_;
+    }
+
+    LIter& operator++()
+    {
+      node_ = node_->next_;
+      return *this;
+    }
+
+    LIter* operator++(int)
+    {
+      LIter tmp = *this;
+      ++(*this);
+      return tmp;
+    }
+
+    bool operator==(const LIter& other) const
+    {
+      return node_ == other.node_;
+    }
+
+    bool operator!=(const LIter& other) const
+    {
+      return !(node_ == other);
+    }
   };
 
   template< class T >
   class List
   {
-    Node* pseudo_;
-    size_t size;
+    Node* pseudoknot_;
+    size_t size_;
+
+    List():
+      pseudoknot_(new Node()),
+      size_(0)
+    {
+      pseudoknot_->next_ = pseudoknot_;
+    }
+
+
   };
 }
 
