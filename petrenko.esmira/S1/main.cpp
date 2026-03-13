888d30cf3109ed10af031eb69e9efdc7dcb57ddd
petrenko.esmira <esmira3942@gmail.com>
2026-03-13 22:11:04 +0300

    create Node

--- petrenko.esmira/S1/main.cpp
+++ petrenko.esmira/S1/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 
-namespace petrenko
-{
+namespace petrenko {
   template< class T > class List;
   template< class T >
   class LIter {
@@ -16,23 +15,40 @@ namespace petrenko
   template< class T >
   class List {
   public:
+    List();
+    ~List();
     size_t GetSize() { return Size; } // возвращает размер списка
     void insert(T data, int index); // добавление элемента по индексу
     void removeAt(int index); // удаление элемента по индексу
     void clear(); // очищение списка
   private:
     size_t Size;
+
+    template<typename T>
+    class Node {
+    public:
+      Node * pNext;
+      T data;
+
+      Node(T data = T(), Node *pNext = nullptr)
+      {
+	this->data = data;
+	this->pNext = pNext;
+      }
+    };
   };
 }
 
 int main()
 {
+  // список списков
+  // список названий
   while (!std::cin.eof()) {
     std::string title;
     std::cin >> title;
     std::cout << title << "название";
     //вставить титле в список титлов
-
+    // список чисел
     int num;
     while (std::cin >> num) {
       //вставить число в список
