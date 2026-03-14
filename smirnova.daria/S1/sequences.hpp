f356101da9397e8a32963f7722626a5a3fc0407d
dassrm <daryasm1rnoff@yandex.ru>
2026-03-14 15:02:00 +0300

    create sequences.hpp with type definitions and function declarations

---
+++ smirnova.daria/S1/sequences.hpp
@@ -0,0 +1,28 @@
+#ifndef SMIRNOVA_SEQUENCES_HPP
+#define SMIRNOVA_SEQUENCES_HPP
+
+#include <string>
+#include <utility>
+
+#include "list.hpp"
+
+namespace smirnova {
+
+using Numbers = List< long long >;
+using NamedSeq = std::pair< std::string, Numbers >;
+using Sequences = List< NamedSeq >;
+using Transposed = List< Numbers >;
+
+Sequences readSequences();
+
+void printNames(const Sequences& seqs);
+void printNumbers(const Numbers& nums);
+
+Transposed buildTransposed(const Sequences& seqs);
+
+long long computeSum(const Numbers& nums);
+void printSums(const Transposed& transposed);
+
+}
+
+#endif
