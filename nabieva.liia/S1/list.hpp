deb5d67536af28f8322f4501b3cb5b6dd0fb0659
liia <kirilich_lubit@rambler.ru>
2026-03-14 06:58:14 +0300

    add node and namespace

--- nabieva.liia/S1/list.hpp
+++ nabieva.liia/S1/list.hpp
@@ -1,19 +1,33 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
-template< class T > class List;
-template < class T >
-class LIter {
-  friend class List< T >;
-};
+namespace nabieva
+{
+  template< class T >
+  struct Node
+  {
+    T data;
+    Node* next;
+    Node(const T& value):
+      data(value),
+      next(nullptr)
+    {}
+  };
 
-template < class T >
-class LCIter {
-  friend class List< T >;
-};
+  template< class T > class List;
+  template < class T >
+  class LIter {
+    friend class List< T >;
+  };
 
-template < class T >
-class List {
-};
+  template < class T >
+  class LCIter {
+    friend class List< T >;
+  };
+
+  template < class T >
+  class List {
+  };
+}
 
 #endif
