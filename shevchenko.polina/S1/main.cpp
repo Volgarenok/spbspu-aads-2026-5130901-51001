d20fcaee6719bdd0b63f5dd9c603754601bc3ea1
pollinnshh <polinashvch248@gmail.com>
2026-03-14 20:07:38 +0300

    add main w try-catch

--- shevchenko.polina/S1/main.cpp
+++ shevchenko.polina/S1/main.cpp
@@ -1,2 +1,38 @@
+#include <iostream>
+#include <new>
+#include "list.hpp"
+#include "io.hpp"
+
 int main()
-{}
+{
+  using namespace shevchenko;
+  
+  try
+  {
+    List< Seq > sequences = readSequences();
+    
+    if (sequences.empty())
+    {
+      std::cout << "0\n";
+      return 0;
+    }
+    
+    printNames(sequences);
+    printTransposed(sequences);
+    
+    List< int > sums = calculateSums(sequences);
+    printSums(sums);
+    
+    return 0;
+  }
+  catch (const std::bad_alloc& e)
+  {
+    std::cerr << "memory error: " << e.what() << "\n";
+    return 1;
+  }
+  catch (...)
+  {
+    std::cerr << "error\n";
+    return 1;
+  }
+}
