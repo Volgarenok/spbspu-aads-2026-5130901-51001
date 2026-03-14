77668f0d189162944359f763beca0150b6e3b7bc
pollinnshh <polinashvch248@gmail.com>
2026-03-14 19:49:25 +0300

    io func realization

--- shevchenko.polina/S1/io.cpp
+++ shevchenko.polina/S1/io.cpp
@@ -1,7 +1,161 @@
-//
-//  io.cpp
-//  
-//
-//  Created by polina shevchenko on 12.03.2026.
-//
+#include "io.hpp"
+#include <iostream>
+#include <cctype>
 
+namespace shevchenko
+{
+List< Seq > readSequences()
+{
+  List< Seq > sequences;
+  std::string name;
+  
+  while (std::cin >> name)
+  {
+    List< int > numbers;
+    int value = 0;
+    
+    while (std::cin.peek() != EOF)
+    {
+      while (std::cin.peek() == ' ')
+      {
+        std::cin.get();
+      }
+      
+      if (std::cin.peek() == EOF || std::isalpha(std::cin.peek()))
+      {
+        break;
+      }
+      
+      if (std::cin >> value)
+      {
+        numbers.pushBack(value);
+      }
+      else
+      {
+        std::cin.clear();
+        std::cin.ignore();
+        return sequences;
+      }
+    }
+    
+    sequences.pushBack({ name, numbers });
+  }
+  
+  return sequences;
+}
+
+void printNames(const List< Sequence >& sequences)
+{
+  bool first = true;
+  
+  for (auto it = sequences.cbegin(); it != sequences.cend(); ++it)
+  {
+    if (!first)
+    {
+      std::cout << " ";
+    }
+    std::cout << (*it).first;
+    first = false;
+  }
+  std::cout << "\n";
+}
+
+void printTransposed(const List< Seq >& sequences)
+{
+  size_t max_len = 0;
+  
+  for (auto it = sequences.cbegin(); it != sequences.cend(); ++it)
+  {
+    size_t s = (*it).second.size();
+    if (s > max_len)
+    {
+      max_len = s;
+    }
+  }
+  
+  for (size_t i = 0; i < max_len; ++i)
+  {
+    bool first = true;
+    
+    for (auto sit = sequences.cbegin(); sit != sequences.cend(); ++sit)
+    {
+      if (!first)
+      {
+        std::cout << " ";
+      }
+      
+      auto nit = (*sit).second.cbegin();
+      for (size_t j = 0; j < i && nit != (*sit).second.cend(); ++j)
+      {
+        ++nit;
+      }
+      
+      if (nit != (*sit).second.cend())
+      {
+        std::cout << *nit;
+      }
+      first = false;
+    }
+    std::cout << "\n";
+  }
+}
+
+List< int > calculateSums(const List< Seq >& sequences)
+{
+  List< int > sums;
+  size_t max_len = 0;
+  
+  for (auto it = sequences.cbegin(); it != sequences.cend(); ++it)
+  {
+    size_t s = (*it).second.size();
+    if (s > max_len)
+    {
+      max_len = s;
+    }
+  }
+  
+  for (size_t i = 0; i < max_len; ++i)
+  {
+    int sum = 0;
+    bool has_number = false;
+    
+    for (auto sit = sequences.cbegin(); sit != sequences.cend(); ++sit)
+    {
+      auto nit = (*sit).second.cbegin();
+      for (size_t j = 0; j < i && nit != (*sit).second.cend(); ++j)
+      {
+        ++nit;
+      }
+      
+      if (nit != (*sit).second.cend())
+      {
+        sum += *nit;
+        has_number = true;
+      }
+    }
+    
+    if (has_number)
+    {
+      sums.pushBack(sum);
+    }
+  }
+  
+  return sums;
+}
+
+void printSums(const List< int >& sums)
+{
+  bool first = true;
+  
+  for (auto it = sums.cbegin(); it != sums.cend(); ++it)
+  {
+    if (!first)
+    {
+      std::cout << " ";
+    }
+    std::cout << *it;
+    first = false;
+  }
+  std::cout << "\n";
+}
+}
