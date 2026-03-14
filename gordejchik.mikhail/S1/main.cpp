59180c9d10bc3dd413683c90726a5ed7906579f3
uncharted-bread <gordeychikmika@gmail.com>
2026-03-14 22:50:33 +0300

    added readInput and printNamed to main.cpp

--- gordejchik.mikhail/S1/main.cpp
+++ gordejchik.mikhail/S1/main.cpp
@@ -1,4 +1,49 @@
+#include <iostream>
+#include <string>
+#include "list.hpp"
+#include "named-list.hpp"
+
+namespace gordejchik {
+  static void readInput(List< NamedList >& namedLists)
+  {
+    std::string name;
+    while (std::cin >> name) {
+      NamedList namedList(name);
+      if (std::cin.eof()) {
+        namedLists.pushBack(static_cast< NamedList&& >(namedList));
+        break;
+      }
+      size_t num = 0;
+      while (std::cin >> num) {
+        namedList.nums.pushBack(num);
+      }
+      namedLists.pushBack(static_cast< NamedList&& >(namedList));
+      std::cin.clear();
+    }
+  }
+
+  static void printNames(const List< NamedList >& namedLists)
+  {
+    bool first = true;
+    for (auto it = namedLists.cbegin(); it != namedLists.cend(); ++it) {
+      if (!first) {
+        std::cout << ' ';
+      }
+      std::cout << it->name;
+      first = false;
+    }
+    std::cout << '\n';
+  }
+}
+
 int main()
 {
-	
+  gordejchik::List< gordejchik::NamedList > namedLists;
+  gordejchik::readInput(namedLists);
+  if (namedLists.empty()) {
+    std::cout << 0 << '\n';
+    return 0;
+  }
+  gordejchik::printNames(namedLists);
+  return 0;
 }
