ac27beca6a31be25b5024f850892e1bd49397d8c
dassrm <daryasm1rnoff@yandex.ru>
2026-03-14 12:51:28 +0300

    fixes

--- smirnova.daria/S0/test.cpp
+++ smirnova.daria/S0/test.cpp
@@ -1,5 +1,6 @@
 #define BOOST_TEST_MODULE S0
-#include <boost/test/included/unit_test.hpp>
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
 #include <sstream>
 #include "credentials.hpp"
 BOOST_AUTO_TEST_CASE(credentials_test)
