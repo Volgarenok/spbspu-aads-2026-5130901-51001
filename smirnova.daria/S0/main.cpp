9b155de967f90061306fed281602b35e5003af62
dassrm <daryasm1rnoff@yandex.ru>
2026-03-14 12:59:28 +0300

    fixes

--- smirnova.daria/S0/main.cpp
+++ smirnova.daria/S0/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "credentials.hpp"
+
+#ifndef BOOST_TEST_MODULE
 int main()
 {
   smirnova::out_credentials(std::cout) << "\n";
 }
+#endif
--- smirnova.daria/S0/test.cpp
+++ smirnova.daria/S0/test.cpp
@@ -1,6 +1,5 @@
 #define BOOST_TEST_MODULE S0
-#define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
+#include <boost/test/included/unit_test.hpp>
 #include <sstream>
 #include "credentials.hpp"
 BOOST_AUTO_TEST_CASE(credentials_test)
