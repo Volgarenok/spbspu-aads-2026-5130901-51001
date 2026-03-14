f5e55433a7e689f649a540eb4a6418161781c7a8
SmirnovaMarya <unlenmaria@gmail.com>
2026-03-14 19:23:22 +0300

    add the user part of the program

--- smirnova.marya/S1/main.cpp
+++ smirnova.marya/S1/main.cpp
@@ -374,3 +374,48 @@ namespace smirnova
     return true;
   }
 }
+
+int main() {
+  using namespace smirnova;
+
+  List< std::pair< std::string, List< size_t > > > sequences;
+  bool overflowDetected = false;
+
+  std::string name;
+
+  while (std::cin >> name) {
+      List< size_t > numbers;
+
+      while (true) {
+          int c = std::cin.peek();
+          if (c == '\n' || c == '\r' || c == EOF) {
+              break;
+          }
+
+          size_t num = 0;
+
+          if (!(std::cin >> num)) {
+              overflowDetected = true;
+              std::cin.clear();
+              std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+              break;
+          }
+
+          numbers.push_back(num);
+      }
+
+      std::cin.ignore(std::numeric_limits< std::streamsize >::max(), '\n');
+      sequences.push_back({name, std::move(numbers)});
+  }
+
+  if (overflowDetected) {
+    std::cerr << "overflow\n";
+    return 1;
+  }
+
+  if (!processSequences(sequences)) {
+    return 1;
+  }
+
+  return 0;
+}
