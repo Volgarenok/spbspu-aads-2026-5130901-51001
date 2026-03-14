9f94d1e9290eaa14351309d1a632d24c2c4f7cdd
liia <kirilich_lubit@rambler.ru>
2026-03-14 07:57:28 +0300

    Add Node constructor and complete class members

--- nabieva.liia/S1/list.hpp
+++ nabieva.liia/S1/list.hpp
@@ -18,15 +18,33 @@ namespace nabieva
   template < class T >
   class LIter {
     friend class List< T >;
+  private:
+    Node<T>* node;
+  public:
+    LIter(Node<T>* n = nullptr):
+      node(n)
+    {}
   };
 
   template < class T >
   class LCIter {
     friend class List< T >;
+  private:
+    const Node<T>* node;
+  public:
+    LCIter(const Node<T>* n = nullptr):
+      node(n)
+    {}
   };
 
   template < class T >
   class List {
+  private:
+    Node<T>* head;
+  public:
+    List():
+      head(nullptr)
+    {}
   };
 }
 
