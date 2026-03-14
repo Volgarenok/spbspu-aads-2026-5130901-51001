3a90c640fc73d5bb630889cd3a934518ce97f3c4
SmirnovaMarya <unlenmaria@gmail.com>
2026-03-14 19:00:21 +0300

    declared the structure and classes

--- smirnova.marya/S1/main.cpp
+++ smirnova.marya/S1/main.cpp
@@ -1,4 +1,37 @@
- #include <iostream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <limits>
+#include <stdexcept>
 
-int main()
-{}
+namespace smirnova
+{
+  template < typename T >
+  struct Node
+  {
+    T data;
+    Node* next;
+    Node* prev;
+    Node(const T& d):
+      data(d),
+      next(nullptr),
+      prev(nullptr) {}
+    Node(T&& d):
+      data(std::move(d)),
+      next(nullptr),
+      prev(nullptr)
+    {}
+  };
+
+  template < typename T >
+  class LIter;
+
+  template < typename T >
+  class LCIter;
+
+  template < typename T >
+  class List;
+
+}
+
