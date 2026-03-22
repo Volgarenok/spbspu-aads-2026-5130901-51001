fe3660571c713e783797978519dd4b9651d70b87
petrenko.esmira <esmira3942@gmail.com>
2026-03-22 16:00:55 +0300

    week fix

--- petrenko.esmira/S1/main.cpp
+++ petrenko.esmira/S1/main.cpp
@@ -1,53 +1,107 @@
 #include <iostream>
+#include <string>
+#include <cctype>
 
 namespace petrenko {
-  template< class T > class List;
+  template<class T> class List;
 
-  template< class T >
+  template<class T>
   class LIter {
-  friend class List< T >;
+    friend class List<T>;
   private:
-    typename List<T>::Node<T>* current;
+    typename List<T>::Node *current;
+    LIter(typename List<T>::Node *node) : current(node) {}
   public:
     LIter() : current(nullptr) {}
-    LIter(const LIter<T>& other) : current(other.current) {} // Копирующий конструктор
-    LIter<T>& operator=(const LIter<T>& other) { // Оператор присваивания
+    LIter(const LIter<T>& other) : current(other.current) {}
+    LIter<T>& operator=(const LIter<T>& other) {
       if (this != &other) {
         current = other.current;
       }
       return *this;
     }
+    LIter<T>& operator++() {
+      if (current) {
+        current = current->pNext;
+      }
+      return *this;
+    }
+    T& operator*() const {
+      return current->data;
+    }
+    T* operator->() const {
+      return &current->data;
+    }
+    bool operator!=(const LIter<T>& other) const {
+      return current != other.current;
+    }
+    bool operator==(const LIter<T>& other) const {
+      return current == other.current;
+    }
   };
 
-  template< class T >
+  template<class T>
   class LCIter {
-  friend class List< T >;
-  //
+    friend class List<T>;
+  private:
+    const typename List<T>::Node *current;
+    LCIter(const typename List<T>::Node *node) : current(node) {}
+  public:
+    LCIter() : current(nullptr) {}
+    LCIter(const LCIter<T>& other) : current(other.current) {}
+    LCIter<T>& operator=(const LCIter<T>& other) {
+      if (this != &other) {
+        current = other.current;
+      }
+      return *this;
+    }
+    LCIter<T>& operator++() {
+      if (current) {
+        current = current->pNext;
+      }
+      return *this;
+    }
+    const T& operator*() const {
+      return current->data;
+    }
+    const T* operator->() const {
+      return &current->data;
+    }
+    bool operator!=(const LCIter<T>& other) const {
+      return current != other.current;
+    }
+    bool operator==(const LCIter<T>& other) const {
+      return current == other.current;
+    }
   };
 
-  template< class T >
+  template<class T>
   class List {
   public:
+    friend class LIter<T>;
+    friend class LCIter<T>;
+
     List();
     ~List();
-    size_t GetSize() { return Size; } // возвращает размер списка
-    void insert(T data, int index); // добавление элемента по индексу
-    void removeAt(int index); // удаление элемента по индексу
-    void clear(); // очищение списка
+    List(const List& other);
+    List& operator=(const List& other);
+
+    size_t getSize() { return Size; }
+    void insert(T data, int index);
+    void removeAt(int index);
+    void clear();
+
     LIter<T> begin() {
       return LIter<T>(head);
     }
-
     LIter<T> end() {
       return LIter<T>(nullptr);
     }
-
     LCIter<T> cbegin() const {
-      return LIter<T>(head);
+      return LCIter<T>(head);
     }
-
-    LСIter<T> сend() const {
-      return LСIter<T>(nullptr);
+    LCIter<T> cend() const {
+      return LCIter<T>(nullptr);
     }
 
   private:
@@ -55,76 +109,94 @@ namespace petrenko {
 
     class Node {
     public:
-      Node * pNext;
+      Node* pNext;
       T data;
-
       Node(T data = T(), Node *pNext = nullptr) {
         this->data = data;
         this->pNext = pNext;
       }
     };
-
-    Node *head;
+    Node* head;
   };
 
-  template< class T >
-  List<T>::List() {
-    Size = 0;
-    head = nullptr;
-  }
-
-  template< class T >
+  template<class T>
   List<T>::~List() {
     clear();
   }
 
-  template< class T >
+  template<class T>
+  List<T>::List(const List<T>& other) : Size(0), head(nullptr) {
+    Node* current = other.head;
+    while (current != nullptr) {
+      insert(current->data, Size);
+      current = current->pNext;
+    }
+  }
+
+  template<class T>
+  List<T>& List<T>::operator=(const List<T>& other) {
+    if (this != &other) {
+      clear();
+      Node* current = other.head;
+      while (current != nullptr) {
+        insert(current->data, Size);
+        current = current->pNext;
+      }
+    }
+    return *this;
+  }
+
+  template<class T>
   void List<T>::insert(T data, int index) {
+    if (index < 0) {
+      return;
+    }
+
     if (index == 0) {
       head = new Node(data, head);
       ++Size;
     } else {
-      Node *previous = this->head;
-
-      for (int i = 0; i < index - 1; i++) {
+      Node* previous = head;
+      for (int i = 0; i < index - 1; ++i) {
         previous = previous->pNext;
       }
-
-      Node *newNode = new Node(data, previous->pNext);
+      Node* newNode = new Node(data, previous->pNext);
       previous->pNext = newNode;
-      Size++;
+      ++Size;
     }
   }
 
-  template< class T >
+  template<class T>
   void List<T>::removeAt(int index) {
+    if (index < 0) {
+      return;
+    }
+
     if (index == 0) {
-      Node *temp = head;
+      Node* temp = head;
       head = head->pNext;
       delete temp;
-      Size--;
+      --Size;
     } else {
-      Node *previous = this->head;
-      for (int i = 0; i < index - 1; i++) {
+      Node* previous = head;
+      for (int i = 0; i < index - 1; ++i) {
         previous = previous->pNext;
       }
-
-      Node *toDelete = previous->pNext;
+      Node* toDelete = previous->pNext;
       previous->pNext = toDelete->pNext;
-
       delete toDelete;
-      Size--;
+      --Size;
     }
   }
 
-  template< class T >
+  template<class T>
   void List<T>::clear() {
-    while (Size) {
-      Node *temp = head;
+    while (head != nullptr) {
+      Node* temp = head;
       head = head->pNext;
       delete temp;
-      Size--;
     }
+    Size = 0;
   }
 }
 
@@ -144,3 +216,4 @@ int main() {
     }
   }
 }
+
