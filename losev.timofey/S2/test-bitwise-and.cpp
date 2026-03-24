f1cbf8fe7f46381875ca0177ee25e9198567e57f
L0syash <losev_timofey07@mail.ru>
2026-03-24 15:55:45 +0300

    Support bitwise and operator

--- losev.timofey/S2/test-bitwise-and.cpp
+++ losev.timofey/S2/test-bitwise-and.cpp
@@ -22,4 +22,15 @@ BOOST_AUTO_TEST_CASE(precedence_test) {
   BOOST_CHECK_EQUAL(losev::evaluateExpression("10 / 2 & 3"), (10 / 2) & 3);
 }
 
+BOOST_AUTO_TEST_CASE(combined_test) {
+  BOOST_CHECK_EQUAL(losev::evaluateExpression("(15 & 7) + (8 & 4)"),
+                    (15 & 7) + (8 & 4));
+  BOOST_CHECK_EQUAL(losev::evaluateExpression("(10 & 6) * (5 & 3)"),
+                    (10 & 6) * (5 & 3));
+  BOOST_CHECK_EQUAL(losev::evaluateExpression("(20 & 15) / 3"),
+                    (20 & 15) / 3);
+  BOOST_CHECK_EQUAL(losev::evaluateExpression("(15 & 7) % 3"),
+                    (15 & 7) % 3);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
