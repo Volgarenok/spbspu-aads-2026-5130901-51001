532ec0e2644d6f79684c27b917b1d59a6b96fcbd
L0syash <losev_timofey07@mail.ru>
2026-03-24 15:49:29 +0300

    Make queue copyable and movable

--- losev.timofey/S2/test-stack-queue.cpp
+++ losev.timofey/S2/test-stack-queue.cpp
@@ -102,4 +102,31 @@ BOOST_AUTO_TEST_CASE(front_empty_test) {
   BOOST_CHECK_THROW(q.front(), std::runtime_error);
 }
 
+BOOST_AUTO_TEST_CASE(copy_test) {
+  losev::Queue<int> q1;
+  q1.push(5);
+  q1.push(10);
+  losev::Queue<int> q2(q1);
+  BOOST_CHECK(q2.pop() == 5);
+  BOOST_CHECK(q2.pop() == 10);
+}
+
+BOOST_AUTO_TEST_CASE(move_test) {
+  losev::Queue<int> q1;
+  q1.push(5);
+  q1.push(10);
+  losev::Queue<int> q2(std::move(q1));
+  BOOST_CHECK(q2.pop() == 5);
+  BOOST_CHECK(q2.pop() == 10);
+  BOOST_CHECK(q1.empty());
+}
+
+BOOST_AUTO_TEST_CASE(clear_test) {
+  losev::Queue<int> q;
+  q.push(5);
+  q.push(10);
+  q.clear();
+  BOOST_CHECK(q.empty());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
