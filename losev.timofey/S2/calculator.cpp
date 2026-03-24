8425befb8453028e1fee8436a4361b35ff83dd7c
L0syash <losev_timofey07@mail.ru>
2026-03-24 15:53:09 +0300

    Parse simple numbers

--- losev.timofey/S2/calculator.cpp
+++ losev.timofey/S2/calculator.cpp
@@ -1,9 +1,49 @@
 #include "calculator.h"
+#include "stack_queue.h"
+#include <sstream>
+#include <cctype>
 
 namespace losev {
 
-int evaluateExpression(const std::string& expression) {
-  return 0;
+namespace {
+  int precedence(char op) {
+    switch (op) {
+      case '+': case '-': return 1;
+      case '*': case '/': case '%': return 2;
+      case '&': return 0;
+      default: return -1;
+    }
+  }
+  
+  bool isOperator(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/' || c == '%' || c == '&';
+  }
+  
+  int applyOperation(int a, int b, char op) {
+    switch (op) {
+      case '+': return a + b;
+      case '-': return a - b;
+      case '*': return a * b;
+      case '/': 
+        if (b == 0) throw std::runtime_error("Division by zero");
+        return a / b;
+      case '%':
+        if (b == 0) throw std::runtime_error("Modulo by zero");
+        return a % b;
+      case '&': return a & b;
+      default: throw std::runtime_error("Unknown operator");
+    }
+  }
+}
+
+int evaluateExpression(const std::string& expr) {
+  int num = 0;
+  for (size_t i = 0; i < expr.length(); ++i) {
+    if (isdigit(expr[i])) {
+      num = num * 10 + (expr[i] - '0');
+    }
+  }
+  return num;
 }
 
 }
--- losev.timofey/S2/test-bitwise-and.cpp
+++ losev.timofey/S2/test-bitwise-and.cpp
@@ -6,6 +6,8 @@ BOOST_AUTO_TEST_SUITE(bitwise_and_tests)
 
 BOOST_AUTO_TEST_CASE(simple_test) {
   BOOST_CHECK_EQUAL(losev::evaluateExpression("5 & 3"), 5 & 3);
+  BOOST_CHECK_EQUAL(losev::evaluateExpression("12 & 10"), 12 & 10);
+  BOOST_CHECK_EQUAL(losev::evaluateExpression("255 & 128"), 255 & 128);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
