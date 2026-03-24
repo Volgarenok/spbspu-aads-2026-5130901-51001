32dc7ff4b430bf35168e75316410b9c446bd948f
L0syash <losev_timofey07@mail.ru>
2026-03-24 15:54:40 +0300

    Handle parentheses and precedence

--- losev.timofey/S2/calculator.cpp
+++ losev.timofey/S2/calculator.cpp
@@ -34,16 +34,88 @@ namespace {
       default: throw std::runtime_error("Unknown operator");
     }
   }
+  
+  std::string infixToPostfix(const std::string& expr) {
+    Stack<char> ops;
+    std::string result;
+    std::istringstream iss(expr);
+    std::string token;
+    
+    while (iss >> token) {
+      if (token.length() == 1 && isOperator(token[0])) {
+        char c = token[0];
+        while (!ops.empty() && ops.top() != '(' &&
+               precedence(ops.top()) >= precedence(c)) {
+          result += ops.pop();
+          result += ' ';
+        }
+        ops.push(c);
+      } else if (token == "(") {
+        ops.push('(');
+      } else if (token == ")") {
+        while (!ops.empty() && ops.top() != '(') {
+          result += ops.pop();
+          result += ' ';
+        }
+        if (ops.empty()) {
+          throw std::runtime_error("Mismatched parentheses");
+        }
+        ops.pop();
+      } else if (token.length() == 1 && isdigit(token[0])) {
+        result += token + ' ';
+      } else {
+        for (size_t i = 0; i < token.length(); ++i) {
+          if (!isdigit(token[i]) && token[i] != '-') {
+            throw std::runtime_error("Invalid number");
+          }
+        }
+        result += token + ' ';
+      }
+    }
+    
+    while (!ops.empty()) {
+      if (ops.top() == '(' || ops.top() == ')') {
+        throw std::runtime_error("Mismatched parentheses");
+      }
+      result += ops.pop();
+      result += ' ';
+    }
+    return result;
+  }
+  
+  int evaluatePostfix(const std::string& postfix) {
+    Stack<int> vals;
+    std::istringstream iss(postfix);
+    std::string token;
+    
+    while (iss >> token) {
+      if (token.length() == 1 && isOperator(token[0])) {
+        int b = vals.pop();
+        int a = vals.pop();
+        vals.push(applyOperation(a, b, token[0]));
+      } else {
+        int num = 0;
+        size_t i = (token[0] == '-') ? 1 : 0;
+        for (; i < token.length(); ++i) {
+          if (!isdigit(token[i])) {
+            throw std::runtime_error("Invalid number");
+          }
+          num = num * 10 + (token[i] - '0');
+        }
+        vals.push((token[0] == '-') ? -num : num);
+      }
+    }
+    
+    int result = vals.pop();
+    if (!vals.empty()) {
+      throw std::runtime_error("Invalid expression");
+    }
+    return result;
+  }
 }
 
 int evaluateExpression(const std::string& expr) {
-  int num = 0;
-  for (size_t i = 0; i < expr.length(); ++i) {
-    if (isdigit(expr[i])) {
-      num = num * 10 + (expr[i] - '0');
-    }
-  }
-  return num;
+  return evaluatePostfix(infixToPostfix(expr));
 }
 
 }
--- losev.timofey/S2/test-bitwise-and.cpp
+++ losev.timofey/S2/test-bitwise-and.cpp
@@ -10,4 +10,16 @@ BOOST_AUTO_TEST_CASE(simple_test) {
   BOOST_CHECK_EQUAL(losev::evaluateExpression("255 & 128"), 255 & 128);
 }
 
+BOOST_AUTO_TEST_CASE(complex_test) {
+  BOOST_CHECK_EQUAL(losev::evaluateExpression("(5 & 3) + 2"), (5 & 3) + 2);
+  BOOST_CHECK_EQUAL(losev::evaluateExpression("12 & 10 & 8"), 12 & 10 & 8);
+  BOOST_CHECK_EQUAL(losev::evaluateExpression("(7 & 3) * 2"), (7 & 3) * 2);
+}
+
+BOOST_AUTO_TEST_CASE(precedence_test) {
+  BOOST_CHECK_EQUAL(losev::evaluateExpression("5 + 3 & 2"), (5 + 3) & 2);
+  BOOST_CHECK_EQUAL(losev::evaluateExpression("5 * 3 & 2"), (5 * 3) & 2);
+  BOOST_CHECK_EQUAL(losev::evaluateExpression("10 / 2 & 3"), (10 / 2) & 3);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
