666aa835baddad80ade7bbefd58fae021874c141
pollinnshh <polinashvch248@gmail.com>
2026-03-26 23:51:50 +0300

    functions stack and gueue done

--- shevchenko.polina/S2/gueue.hpp
+++ shevchenko.polina/S2/gueue.hpp
@@ -11,12 +11,53 @@ class Queue
 {
 public:
   void push(const T& value)
+  {
+    data_.pushBack(value);
+  }
+  
   void push(T&& value)
+  {
+    data_.pushBack(std::move(value));
+  }
+  
   T pop()
+  {
+    if (empty())
+    {
+      throw std::logic_error("queue is empty");
+    }
+    T value = data_.front();
+    data_.popFront();
+    return value;
+  }
+  
   T& front()
+  {
+    if (empty())
+    {
+      throw std::logic_error("queue is empty");
+    }
+    return data_.front();
+  }
+  
   const T& front() const
+  {
+    if (empty())
+    {
+      throw std::logic_error("queue is empty");
+    }
+    return data_.front();
+  }
+  
   bool empty() const
+  {
+    return data_.empty();
+  }
+  
   size_t size() const
+  {
+    return data_.size();
+  }
   
 private:
   List< T > data_;
--- shevchenko.polina/S2/stack.hpp
+++ shevchenko.polina/S2/stack.hpp
@@ -11,14 +11,56 @@ class Stack
 {
 public:
   void push(const T& value)
+  {
+    data_.pushFront(value);
+  }
+  
   void push(T&& value)
+  {
+    data_.pushFront(std::move(value));
+  }
+  
   T pop()
+  {
+    if (empty())
+    {
+      throw std::logic_error("stack is empty");
+    }
+    T value = data_.front();
+    data_.popFront();
+    return value;
+  }
+  
   T& top()
+  {
+    if (empty())
+    {
+      throw std::logic_error("stack is empty");
+    }
+    return data_.front();
+  }
+  
   const T& top() const
+  {
+    if (empty())
+    {
+      throw std::logic_error("stack is empty");
+    }
+    return data_.front();
+  }
+  
   bool empty() const
+  {
+    return data_.empty();
+  }
+  
   size_t size() const
+  {
+    return data_.size();
+  }
   
 private:
+  List< T > data_;
 };
 }
 
