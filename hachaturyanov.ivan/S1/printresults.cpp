c83b66343eca7b857d0e79132887fbf67c9e38ea
inshady <inshadyprod@gmail.com>
2026-03-29 19:09:44 +0300

    printNames impl

---
+++ hachaturyanov.ivan/S1/printresults.cpp
@@ -0,0 +1,19 @@
+#include "printresults.hpp"
+
+void hachaturyanov::printNames(std::ostream &out, const List< Pair > &pairs)
+{
+  bool start = true;
+  citer first = pairs.begin();
+  citer it = first;
+  do {
+    if (!start) {
+      out << ' ';
+    }
+
+    out << *it;
+
+    start = false;
+    ++it;
+  } while (it != pairs.begin());
+  out << '\n';
+}
