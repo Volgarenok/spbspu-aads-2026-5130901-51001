23a88ac34a8751c71b0b8338f0f9c7980b34ab95
inshady <inshadyprod@gmail.com>
2026-04-14 01:57:56 +0300

    queue.push() and .drop() implementation

---
+++ hachaturyanov.ivan/S2/queue.hpp
@@ -0,0 +1,28 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+
+#include "../common/list.hpp"
+
+template< class T > class Queue {
+  List< T > data_;
+ public:
+  void push(const T &value);
+  T drop();
+};
+
+template< class T > void Queue< T >::push(const T &value)
+{
+  data_.addEnd(value);
+}
+
+template< class T > T Queue< T >::drop()
+{
+  if (data_.isEmpty()) {
+    throw std::logic_error("Queue is empty");
+  }
+  T res = *(data_.begin());
+  data_.popFront();
+  return res;
+}
+
+#endif
