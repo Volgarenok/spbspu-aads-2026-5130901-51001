825233210aa54608936461984190c42aa6e03bee
inshady <inshadyprod@gmail.com>
2026-04-13 18:58:24 +0300

    rest of tests

--- hachaturyanov.ivan/S1/test-list.cpp
+++ hachaturyanov.ivan/S1/test-list.cpp
@@ -49,6 +49,7 @@ BOOST_AUTO_TEST_CASE(test_insert_before)
   BOOST_TEST(*it == 2);
   BOOST_CHECK(list.size() == 2);
   BOOST_TEST(*list.begin() == 2);
+  ++it;
   BOOST_TEST(*it == 1);
 }
 
@@ -59,7 +60,9 @@ BOOST_AUTO_TEST_CASE(test_insert_after)
   list.insertAfter(it, 2);
   BOOST_CHECK(list.size() == 2);
   BOOST_TEST(*list.begin() == 1);
-  BOOST_TEST(*list.end() == 2);
+  auto end_it = list.end();
+  --end_it;
+  BOOST_TEST(*end_it == 2);
 }
 
 BOOST_AUTO_TEST_CASE(test_pop_front)
@@ -72,3 +75,102 @@ BOOST_AUTO_TEST_CASE(test_pop_front)
   BOOST_TEST(*list.begin() == 2);
 }
 
+BOOST_AUTO_TEST_CASE(test_pop_end)
+{
+  List< int > list;
+  list.addEnd(1);
+  list.addEnd(2);
+  list.popEnd();
+  BOOST_TEST(list.size() == 1);
+  BOOST_TEST(*list.begin() == 1);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_erase)
+{
+  List< int > list;
+  auto it = list.addEnd(1);
+  list.addEnd(2);
+  list.erase(it);
+  BOOST_TEST(list.size() == 1);
+  BOOST_TEST(*list.begin() == 2);
+}
+
+BOOST_AUTO_TEST_CASE(test_clear)
+{
+  List< int > list;
+  list.addEnd(1);
+  list.addEnd(2);
+  list.clear();
+  BOOST_TEST(list.isEmpty());
+}
+
+BOOST_AUTO_TEST_CASE(test_clear_range)
+{
+  List< int > list;
+  auto it1 = list.addEnd(1);
+  auto it2 = list.addEnd(2);
+  list.addEnd(3);
+  list.clear(it1, it2);
+  BOOST_TEST(list.size() == 1);
+  BOOST_TEST(*list.begin() == 3);
+}
+
+BOOST_AUTO_TEST_CASE(test_copy_constructor)
+{
+  List< int > list;
+  list.addEnd(1);
+  list.addEnd(2);
+  List< int > copy(list);
+  BOOST_TEST(copy.size() == list.size());
+  BOOST_TEST(*copy.begin() == *list.begin());
+  auto lit = list.begin();
+  ++lit;
+  auto cit = copy.begin();
+  ++cit;
+  BOOST_TEST(*lit == *cit);
+}
+
+BOOST_AUTO_TEST_CASE(test_assignment_operator)
+{
+  List< int > list;
+  list.addEnd(1);
+  list.addEnd(2);
+  List< int > copy;
+  copy = list;
+  BOOST_TEST(copy.size() == list.size());
+  BOOST_TEST(*copy.begin() == *list.begin());
+  auto lit = list.begin();
+  ++lit;
+  auto cit = copy.begin();
+  ++cit;
+  BOOST_TEST(*lit == *cit);
+}
+
+BOOST_AUTO_TEST_CASE(test_self_assignment)
+{
+  List< int > list;
+  list.addEnd(1);
+  list.addEnd(2);
+  list = list;
+  BOOST_TEST(list.size() == 2);
+  BOOST_TEST(*list.begin() == 1);
+  auto it = list.begin();
+  ++it;
+  BOOST_TEST(*it == 2);
+}
+
+BOOST_AUTO_TEST_CASE(test_move_assignment)
+{
+  List< int > list;
+  list.addEnd(1);
+  list.addEnd(2);
+  List< int > moved;
+  moved = std::move(list);
+  BOOST_TEST(moved.size() == 2);
+  BOOST_TEST(*moved.begin() == 1);
+  auto it = moved.begin();
+  ++it;
+  BOOST_TEST(*it == 2);
+  BOOST_TEST(list.isEmpty());
+}
