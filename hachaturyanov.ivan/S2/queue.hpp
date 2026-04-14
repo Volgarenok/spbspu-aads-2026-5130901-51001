91bd867ffe6b4c5ccff5ddebc3de185c18ea9bdd
inshady <inshadyprod@gmail.com>
2026-04-14 03:22:59 +0300

    stack tests

--- hachaturyanov.ivan/S2/queue.hpp
+++ hachaturyanov.ivan/S2/queue.hpp
@@ -22,59 +22,59 @@ namespace hachaturyanov
     bool isEmpty() const noexcept;
   };
 
-template< class T > T& Queue< T >::begin()
-{
-  return *(data_.begin());
-}
-
-template< class T > const T& Queue< T >::begin() const
-{
-  return *(data_.begin());
-}
-
-template< class T > T& Queue< T >::end()
-{
-  auto it = data_.end();
-  --it;
-  return *it;
-}
-
-template< class T > const T& Queue< T >::end() const
-{
-  auto it = data_.end();
-  --it;
-  return *it;
-}
-
-template< class T > void Queue< T >::push(T &&value)
-{
-  data_.addEnd(std::move(value));
-}
-
-template< class T > bool Queue< T >::isEmpty() const noexcept
-{
-  return data_.isEmpty();
-}
-
-template< class T > size_t Queue< T >::size() const noexcept
-{
-  return data_.size();
-}
-
-template< class T > void Queue< T >::push(const T &value)
-{
-  data_.addEnd(value);
-}
-
-template< class T > T Queue< T >::drop()
-{
-  if (data_.isEmpty()) {
-    throw std::logic_error("Queue is empty");
+  template< class T > T& Queue< T >::begin()
+  {
+    return *(data_.begin());
   }
-  T res = begin();
-  data_.popFront();
-  return res;
-}
+
+  template< class T > const T& Queue< T >::begin() const
+  {
+    return *(data_.begin());
+  }
+
+  template< class T > T& Queue< T >::end()
+  {
+    auto it = data_.end();
+    --it;
+    return *it;
+  }
+
+  template< class T > const T& Queue< T >::end() const
+  {
+    auto it = data_.end();
+    --it;
+    return *it;
+  }
+
+  template< class T > void Queue< T >::push(T &&value)
+  {
+    data_.addEnd(std::move(value));
+  }
+
+  template< class T > bool Queue< T >::isEmpty() const noexcept
+  {
+    return data_.isEmpty();
+  }
+
+  template< class T > size_t Queue< T >::size() const noexcept
+  {
+    return data_.size();
+  }
+
+  template< class T > void Queue< T >::push(const T &value)
+  {
+    data_.addEnd(value);
+  }
+
+  template< class T > T Queue< T >::drop()
+  {
+    if (data_.isEmpty()) {
+      throw std::logic_error("Queue is empty");
+    }
+    T res = begin();
+    data_.popFront();
+    return res;
+  }
 
 }
 
--- hachaturyanov.ivan/S2/test-queue.cpp
+++ hachaturyanov.ivan/S2/test-queue.cpp
@@ -1,5 +1,4 @@
-#define BOOST_TEST_MODULE S2
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 
 #include "queue.hpp"
 
