49c88b0e6b94dab3f28507d593e1ae6f5715a80b
L0syash <losev_timofey07@mail.ru>
2026-04-14 13:50:12 +0300

    fix main

--- losev.timofey/S2/main.cpp
+++ losev.timofey/S2/main.cpp
@@ -8,6 +8,7 @@ int main(int argc, char* argv[]) {
   try {
     std::istream* input = &std::cin;
     std::ifstream file;
+
     if (argc >= 2) {
       file.open(argv[1]);
       if (!file.is_open()) {
@@ -16,13 +17,15 @@ int main(int argc, char* argv[]) {
       }
       input = &file;
     }
+
     losev::List<std::string> expressions;
-    losev::List<int> results;
     std::string line;
+
     while (std::getline(*input, line)) {
       if (line.empty()) {
         continue;
       }
+
       size_t start = 0;
       while (start < line.length() && (line[start] == ' ' || line[start] == '\t')) {
         ++start;
@@ -36,6 +39,7 @@ int main(int argc, char* argv[]) {
         expressions.push_front(line.substr(start, end - start));
       }
     }
+
     if (expressions.empty()) {
       std::cout << std::endl;
       return 0;
@@ -45,10 +49,12 @@ int main(int argc, char* argv[]) {
          it != expressions.end(); ++it) {
       reversed.push_front(*it);
     }
+    losev::List<long long> results;
+
     for (losev::List<std::string>::iterator it = reversed.begin();
          it != reversed.end(); ++it) {
       try {
-        int result = losev::evaluateExpression(*it);
+        long long result = losev::evaluateExpression(*it);
         results.push_front(result);
       } catch (const std::exception& e) {
         std::cerr << "Error: " << *it << " - " << e.what() << std::endl;
@@ -56,7 +62,7 @@ int main(int argc, char* argv[]) {
       }
     }
     bool first = true;
-    for (losev::List<int>::iterator it = results.begin();
+    for (losev::List<long long>::iterator it = results.begin();
          it != results.end(); ++it) {
       if (!first) {
         std::cout << ' ';
@@ -65,6 +71,7 @@ int main(int argc, char* argv[]) {
       first = false;
     }
     std::cout << std::endl;
+
   } catch (const std::exception& e) {
     std::cerr << "Fatal error: " << e.what() << std::endl;
     return 1;
