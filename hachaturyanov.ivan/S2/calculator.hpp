f44ee7c1f533dd72b0026f456741e933113e3b15
inshady <inshadyprod@gmail.com>
2026-04-20 03:10:29 +0300

    main

---
+++ hachaturyanov.ivan/S2/calculator.hpp
@@ -0,0 +1,11 @@
+#ifndef CALCULATOR_HPP
+#define CALCULATOR_HPP
+
+#include <iostream>
+
+namespace hachaturyanov
+{
+  int calculate(std::istream& input);
+}
+
+#endif
--- hachaturyanov.ivan/S2/main.cpp
+++ hachaturyanov.ivan/S2/main.cpp
@@ -1,2 +1,26 @@
-int main()
-{}
+#include <iostream>
+#include <stdexcept>
+#include <fstream>
+#include "calculator.hpp"
+
+int main(int argc, char* argv[])
+{
+  try {
+    if (argc == 2) {
+      std::ifstream input(argv[1]);
+      if (!input) {
+        std::cerr << "Failed to open file: " << argv[1] << '\n';
+        return 1;
+      }
+      return hachaturyanov::calculate(input);
+    } else if (argc == 1) {
+      return hachaturyanov::calculate(std::cin);
+    } else {
+      std::cerr << "Invalid arguments\n";
+      return 1;
+    }
+  } catch (const std::exception& e) {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+}
