17bf931793ccf191a8e89d39dfc692629c270c3d
RBorisov6 <borisovruslan1512@gmail.com>
2026-04-21 20:20:10 +0300

    fix main file

--- borisov.ruslan/S1/main.cpp
+++ borisov.ruslan/S1/main.cpp
@@ -2,5 +2,7 @@
 #include <iostream>
 
 int main() {
-  return borisov::run(std::cin, std::cout, std::cerr);
+  int result = borisov::run(std::cin, std::cout, std::cerr);
+  if (result != 0) return 1;
+  return 0;
 }
