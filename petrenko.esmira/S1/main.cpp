e813c4eaaf4394c3840458d594063af75ebd9ecf
petrenko.esmira <esmira3942@gmail.com>
2026-04-27 14:42:41 +0300

    only s2

--- petrenko.esmira/S1/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "processor.hpp"
-
-int main() {
-  petrenko::ProcessResult result = petrenko::processInput(std::cin);
-  petrenko::printResults(std::cout, std::cerr, result);
-  return result.overflow ? 1 : 0;
-}
