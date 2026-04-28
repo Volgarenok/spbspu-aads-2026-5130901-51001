b3c449d63ff09d4c010f2b73eb81b284a5c7c13c
RBorisov6 <borisovruslan1512@gmail.com>
2026-04-28 13:42:51 +0300

    add tests for basic queue

---
+++ borisov.ruslan/S2/test-queue.cpp
@@ -0,0 +1,56 @@
+#include <boost/test/unit_test.hpp>
+#include "queue.hpp"
+
+BOOST_AUTO_TEST_CASE(queue_push_and_front)
+{
+  borisov::Queue<int> q;
+  q.push(10);
+  BOOST_CHECK_EQUAL(q.front(), 10);
+  q.push(20);
+  BOOST_CHECK_EQUAL(q.front(), 10);  // front remains first added
+}
+
+BOOST_AUTO_TEST_CASE(queue_pop_fifo_order)
+{
+  borisov::Queue<int> q;
+  q.push(1);
+  q.push(2);
+  q.pop();
+  BOOST_CHECK_EQUAL(q.front(), 2);
+  q.pop();
+  BOOST_CHECK(q.empty());
+}
+
+BOOST_AUTO_TEST_CASE(queue_back_reflects_last)
+{
+  borisov::Queue<int> q;
+  q.push(100);
+  q.push(200);
+  BOOST_CHECK_EQUAL(q.back(), 200);
+  q.pop();
+  BOOST_CHECK_EQUAL(q.back(), 200); // back unchanged after pop front
+}
+
+BOOST_AUTO_TEST_CASE(queue_empty_initially)
+{
+  borisov::Queue<int> q;
+  BOOST_CHECK(q.empty());
+}
+
+BOOST_AUTO_TEST_CASE(queue_pop_on_empty_throws)
+{
+  borisov::Queue<int> q;
+  BOOST_CHECK_THROW(q.pop(), std::logic_error);
+}
+
+BOOST_AUTO_TEST_CASE(queue_front_on_empty_throws)
+{
+  borisov::Queue<int> q;
+  BOOST_CHECK_THROW(q.front(), std::logic_error);
+}
+
+BOOST_AUTO_TEST_CASE(queue_back_on_empty_throws)
+{
+  borisov::Queue<int> q;
+  BOOST_CHECK_THROW(q.back(), std::logic_error);
+}
