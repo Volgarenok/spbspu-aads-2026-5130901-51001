2f6d76770fee76e1aead0cfc0bacb2e85e07feb0
RBorisov6 <borisovruslan1512@gmail.com>
2026-04-28 13:40:10 +0300

    tests for copy and move semantic

--- borisov.ruslan/S2/test-stack.cpp
+++ borisov.ruslan/S2/test-stack.cpp
@@ -69,3 +69,69 @@ BOOST_AUTO_TEST_CASE(stack_clear_makes_empty)
   BOOST_CHECK(s.empty());
   BOOST_CHECK_EQUAL(s.size(), 0u);
 }
+
+BOOST_AUTO_TEST_CASE(stack_copy_constructor_creates_independent_copy)
+{
+  borisov::Stack<int> original;
+  original.push(10);
+  original.push(20);
+  borisov::Stack<int> copy(original);
+  BOOST_CHECK_EQUAL(copy.size(), original.size());
+  BOOST_CHECK_EQUAL(copy.top(), original.top());
+  copy.pop();
+  BOOST_CHECK_EQUAL(copy.size(), original.size() - 1);
+  BOOST_CHECK_EQUAL(original.top(), 20); // original unchanged
+}
+
+BOOST_AUTO_TEST_CASE(stack_copy_assignment_replaces_content)
+{
+  borisov::Stack<int> s1;
+  s1.push(1);
+  s1.push(2);
+
+  borisov::Stack<int> s2;
+  s2.push(100);
+
+  s2 = s1;
+  BOOST_CHECK_EQUAL(s2.size(), 2);
+  BOOST_CHECK_EQUAL(s2.top(), 2);
+  s1.pop();
+  BOOST_CHECK_EQUAL(s2.size(), 2); // s2 independent
+}
+
+BOOST_AUTO_TEST_CASE(stack_move_constructor_transfers_ownership)
+{
+  borisov::Stack<int> source;
+  source.push(42);
+  source.push(99);
+  borisov::Stack<int> target(std::move(source));
+  BOOST_CHECK(source.empty());
+  BOOST_CHECK_EQUAL(target.size(), 2);
+  BOOST_CHECK_EQUAL(target.top(), 99);
+}
+
+BOOST_AUTO_TEST_CASE(stack_move_assignment_transfers_ownership)
+{
+  borisov::Stack<int> source;
+  source.push(7);
+
+  borisov::Stack<int> target;
+  target.push(1000);
+
+  target = std::move(source);
+  BOOST_CHECK(source.empty());
+  BOOST_CHECK_EQUAL(target.size(), 1);
+  BOOST_CHECK_EQUAL(target.top(), 7);
+}
+
+BOOST_AUTO_TEST_CASE(stack_self_assignment_does_nothing)
+{
+  borisov::Stack<int> s;
+  s.push(5);
+  s = s;
+  BOOST_CHECK_EQUAL(s.size(), 1);
+  BOOST_CHECK_EQUAL(s.top(), 5);
+  s = std::move(s);
+  BOOST_CHECK_EQUAL(s.size(), 1);
+  BOOST_CHECK_EQUAL(s.top(), 5);
+}
