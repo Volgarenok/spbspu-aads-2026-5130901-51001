87c09ec153a5b9fba3dbb08a37c5fd7826c0d7bc
swagainator <platonalekseevi6@gmail.com>
2026-05-05 22:58:39 +0300

    feat(s3): add lab entrypoint and file argument validation

--- alekseev.platon/S3/main.cpp
+++ alekseev.platon/S3/main.cpp
@@ -1,2 +1,33 @@
-int main()
-{}
+#include <fstream>
+#include <iostream>
+
+namespace alekseev
+{
+  bool load_graphs(std::istream&)
+  {
+    return true;
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc != 2)
+  {
+    std::cerr << "filename is required\n";
+    return 1;
+  }
+
+  std::ifstream input(argv[1]);
+  if (!input)
+  {
+    std::cerr << "cannot open file\n";
+    return 1;
+  }
+
+  if (!alekseev::load_graphs(input))
+  {
+    return 1;
+  }
+
+  return 0;
+}
