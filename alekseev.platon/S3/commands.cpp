fe18472d3392522d1b905bb6649fdf7613d48145
swagainator <platonalekseevi6@gmail.com>
2026-05-05 23:06:25 +0300

    feat(s3): implement graph listing and edge query commands

--- alekseev.platon/S3/commands.cpp
+++ alekseev.platon/S3/commands.cpp
@@ -4,55 +4,113 @@ namespace alekseev
 {
   namespace
   {
-    void stub_invalid(const Sequence< std::string >&, GraphStorage&, std::ostream& out)
+    void print_strings(const Sequence< std::string >& values, std::ostream& out)
     {
-      print_invalid(out);
+      for (size_t i = 0; i < values.size(); ++i)
+      {
+        out << values[i] << '\n';
+      }
+    }
+
+    void print_edge_lines(const Sequence< EdgeQueryLine >& lines, std::ostream& out)
+    {
+      for (size_t i = 0; i < lines.size(); ++i)
+      {
+        out << lines[i].vertex;
+        for (size_t j = 0; j < lines[i].weights.size(); ++j)
+        {
+          out << ' ' << lines[i].weights[j];
+        }
+        out << '\n';
+      }
     }
   }
 
   void handle_graphs(const Sequence< std::string >& args, GraphStorage& storage, std::ostream& out)
   {
-    stub_invalid(args, storage, out);
+    if (args.size() != 1)
+    {
+      print_invalid(out);
+      return;
+    }
+    print_strings(storage.graph_names_sorted(), out);
   }
 
   void handle_vertexes(const Sequence< std::string >& args, GraphStorage& storage, std::ostream& out)
   {
-    stub_invalid(args, storage, out);
+    if (args.size() != 2 || !storage.has_graph(args[1]))
+    {
+      print_invalid(out);
+      return;
+    }
+    print_strings(storage.get_graph(args[1]).vertexes_sorted(), out);
   }
 
   void handle_outbound(const Sequence< std::string >& args, GraphStorage& storage, std::ostream& out)
   {
-    stub_invalid(args, storage, out);
+    if (args.size() != 3 || !storage.has_graph(args[1]))
+    {
+      print_invalid(out);
+      return;
+    }
+    const Graph& graph = storage.get_graph(args[1]);
+    if (!graph.has_vertex(args[2]))
+    {
+      print_invalid(out);
+      return;
+    }
+    print_edge_lines(graph.outbound_sorted(args[2]), out);
   }
 
   void handle_inbound(const Sequence< std::string >& args, GraphStorage& storage, std::ostream& out)
   {
-    stub_invalid(args, storage, out);
+    if (args.size() != 3 || !storage.has_graph(args[1]))
+    {
+      print_invalid(out);
+      return;
+    }
+    const Graph& graph = storage.get_graph(args[1]);
+    if (!graph.has_vertex(args[2]))
+    {
+      print_invalid(out);
+      return;
+    }
+    print_edge_lines(graph.inbound_sorted(args[2]), out);
   }
 
   void handle_bind(const Sequence< std::string >& args, GraphStorage& storage, std::ostream& out)
   {
-    stub_invalid(args, storage, out);
+    (void) args;
+    (void) storage;
+    print_invalid(out);
   }
 
   void handle_cut(const Sequence< std::string >& args, GraphStorage& storage, std::ostream& out)
   {
-    stub_invalid(args, storage, out);
+    (void) args;
+    (void) storage;
+    print_invalid(out);
   }
 
   void handle_create(const Sequence< std::string >& args, GraphStorage& storage, std::ostream& out)
   {
-    stub_invalid(args, storage, out);
+    (void) args;
+    (void) storage;
+    print_invalid(out);
   }
 
   void handle_merge(const Sequence< std::string >& args, GraphStorage& storage, std::ostream& out)
   {
-    stub_invalid(args, storage, out);
+    (void) args;
+    (void) storage;
+    print_invalid(out);
   }
 
   void handle_extract(const Sequence< std::string >& args, GraphStorage& storage, std::ostream& out)
   {
-    stub_invalid(args, storage, out);
+    (void) args;
+    (void) storage;
+    print_invalid(out);
   }
 
   CommandTable make_command_table()
