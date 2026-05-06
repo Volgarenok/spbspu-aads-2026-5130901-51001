4676b9d7fc4e243824e328fc8eb16f98397a924f
restorte <vanyakarpenko837@yandex.ru>
2026-05-06 19:39:01 +0300

    feat(Vector): add class declaration and basic constructors

---
+++ karpenko.ivan/common/hash_table.hpp
@@ -0,0 +1,34 @@
+#ifndef HASH_TABLE_HPP
+#define HASH_TABLE_HPP
+
+#include <algorithm>
+#include <cstddef>
+#include <initializer_list>
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+namespace karpenko
+{
+
+template <class T>
+struct Vector
+{
+  Vector();
+  Vector(size_t s, const T& val);
+  ~Vector();
+
+  Vector(std::initializer_list<T> il);
+
+  Vector(Vector<T>&&) noexcept;
+  Vector<T>& operator=(Vector<T>&&) noexcept;
+  void pushFront(const T& val);
+  void swap(Vector<T>&) noexcept;
+  Vector(const Vector<T>&);
+  Vector<T>& operator=(const Vector<T>&);
+  bool isEmpty() const noexcept;
+};
+
+}
+
+#endif
