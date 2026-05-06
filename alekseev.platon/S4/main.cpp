9f7ad207f22c5a2eea8da501e811b5f7287099fc
swagainator <platonalekseevi6@gmail.com>
2026-05-06 11:24:13 +0300

    wire main command loop

--- alekseev.platon/S4/main.cpp
+++ alekseev.platon/S4/main.cpp
@@ -6,7 +6,46 @@
 #include "commands.hpp"
 #include "dataset.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
-  return 0;
+  try
+  {
+    if (argc != 2)
+    {
+      std::cerr << "filename is required\n";
+      return 1;
+    }
+
+    std::ifstream input(argv[1]);
+    if (!input)
+    {
+      std::cerr << "cannot open file\n";
+      return 1;
+    }
+
+    alekseev::DictionaryStorage storage;
+    if (!alekseev::loadDatasets(input, storage))
+    {
+      std::cerr << "invalid file\n";
+      return 1;
+    }
+
+    const alekseev::CommandTable commands = alekseev::makeCommandTable();
+    std::string line;
+    while (std::getline(std::cin, line))
+    {
+      alekseev::dispatchCommand(commands, line, storage, std::cout);
+    }
+    return 0;
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "internal error: " << e.what() << '\n';
+    return 2;
+  }
+  catch (...)
+  {
+    std::cerr << "internal error\n";
+    return 2;
+  }
 }
