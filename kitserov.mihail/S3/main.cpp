f34fc11eaadec09505d78bcf6d8ad332dbc898ab
Kreniker <kitserov.mike@gmail.com>
2026-05-11 18:01:46 +0300

    blank main for cmd-funcs

--- kitserov.mihail/S3/main.cpp
+++ kitserov.mihail/S3/main.cpp
@@ -6,6 +6,7 @@
 int main(int argc, char* argv[])
 {
   using namespace kitserov;
+  using CommandHandler = void (*)(std::ostream& out, std::istream& in);
   if (argc != 2) {
     std::cerr << "Must be 2 argc\n";
     return 1;
@@ -15,7 +16,7 @@ int main(int argc, char* argv[])
     std::cerr << "Error: cannot open file \"" << argv[1] << "\"" << std::endl;
     return 1;
   }
-  std::vector< Graph > graphs;
+  std::vector< std::string, Graph > graphs;
   std::string word;
   size_t countEdges = 0;
   while (inputFile >> word) {
@@ -25,9 +26,9 @@ int main(int argc, char* argv[])
         std::cerr << "Error: expected edge count after graph name\n";
         return 2;
       }
-      graphs.push_back(std::move(g));
+      graphs.add(word, std::move(g));
     } else {
-      Graph& g = graphs.back();
+      Graph& g = *(graphs.begin());
       std::string src = word;
       std::string dst;
       size_t weight = 0;
@@ -40,22 +41,32 @@ int main(int argc, char* argv[])
     }
   }
 
-  HashTable< std::string, void(*)(std::ostream&, std::istream&), SipHash< std::string >, std::equal_to< std::string > > cmds(20);
+  HashTable< std::string, CommandHandler, SipHash< std::string > > cmds(20);
   
-  cmds.add("graphs", graphs);
-  cmds.add("vertexes", vertexes);
-  cmds.add("outbound", outbound);
-  cmds.add("inbound", inbound);
-  cmds.add("", );  
+  cmds.add("graphs", cmd_graphs);
+  cmds.add("vertexes", cmd_vertexes);
+  cmds.add("outbound", cmd_outbound);
+  cmds.add("inbound", cmd_inbound);
+  cmds.add("bind", cmd_bind);
+  cmds.add("cut", cmd_cut);
+  cmds.add("create", cmd_create);
+  cmds.add("merge", cmd_merge);
+  cmds.add("extract", cmd_extract);
 
   std::string cmd;
   while (std::cin >> cmd)
   {
     try {
-      cmds.at(cmd)(std::cout, std::cin);
+      auto handler = cmds.find(cmd);
+      if (!handler) {
+        throw std::invalid_argument("unknown command");
+      }
+      (*handler)(std::cout, std::cin);
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     } catch (...) {
-      std::cerr << "Error\n";
-      return 2;
+      std::cout << "<INVALID COMMAND>\n";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
   }
   return 0;
