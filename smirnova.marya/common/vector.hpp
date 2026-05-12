c53bc12d092505189a3efe5d3f3f6c97e0d47372
SmirnovaMarya <unlenmaria@gmail.com>
2026-05-12 15:21:18 +0300

    moved the iterators to another file

--- smirnova.marya/common/vector.hpp
+++ smirnova.marya/common/vector.hpp
@@ -2,137 +2,130 @@
 #define VECTOR_HPP
 
 #include <cstddef>
-#include <utility>
+#include "ector-Iterator.hpp"
 
 namespace smirnova
 {
 
-  template <typename T>
-  class Vector
+template <typename T>
+class Vector
+{
+private:
+  T* data;
+  size_t sz;
+  size_t cap;
+
+  void expansion(size_t newCap);
+  void ensureCapacity();
+
+public:
+  Vector()
+    : data(0), sz(0), cap(0)
+  {}
+
+  ~Vector()
   {
-  private:
-    T* data;
-    size_t sz;
-    size_t cap;
+    delete[] data;
+  }
 
-    void expansion(size_t newCap)
-    {
-      if (newCap <= cap)
-        return;
+  size_t size() const;
 
-      T* newData = new T[newCap];
+  T& operator[](size_t i);
+  const T& operator[](size_t i) const;
 
-      for (size_t i = 0; i < sz; ++i)
-      {
-        newData[i] = std::move(data[i]);
-      }
+  void pushBack(const T& v);
+  void pushBack(T&& v);
 
-      delete[] data;
-      data = newData;
-      cap = newCap;
-    }
+  VectorIterator<T> begin();
+  VectorIterator<T> end();
 
-  public:
-    Vector():
-      data(nullptr), sz(0), cap(0)
-    {}
-
-    Vector(const Vector& other):
-      data(nullptr), sz(other.sz), cap(other.cap)
-    {
-      if (cap > 0)
-        data = new T[cap];
-
-      for (size_t i = 0; i < sz; ++i)
-        data[i] = other.data[i];
-    }
-
-    Vector& operator=(const Vector& other)
-    {
-      if (this != &other)
-      {
-        Vector tmp(other);
-        std::swap(data, tmp.data);
-        std::swap(sz, tmp.sz);
-        std::swap(cap, tmp.cap);
-      }
-      return *this;
-    }
-
-    ~Vector()
-    {
-      delete[] data;
-    }
-
-    size_t size() const
-    {
-      return sz;
-    }
-
-    T& operator[](size_t i)
-    {
-      return data[i];
-    }
-    
-    const T& operator[](size_t i) const
-    {
-      return data[i];
-    }
-
-    void pushBack(const T& v)
-    {
-      if (sz == cap)
-      {
-        expansion(cap == 0 ? 2 : cap * 2);
-      }
-      data[sz++] = v;
-    }
-
-    void pushBack(T&& v)
-    {
-      if (sz == cap)
-      {
-        expansion(cap == 0 ? 2 : cap * 2);
-      }
-      data[sz++] = std::move(v);
-    }
-
-    class iterator
-    {
-      T* ptr;
-
-    public:
-      iterator(T* p = nullptr):
-        ptr(p)
-      {}
-
-      T& operator*()
-      {
-        return *ptr;
-      }
-
-      iterator& operator++()
-      {
-        ++ptr;
-        return *this;
-      }
-
-      bool operator!=(const iterator& other) const
-      {
-        return ptr != other.ptr;
-      }
-    };
-
-    iterator begin()
-    {
-      return iterator(data);
-    }
-    iterator end()
-    {
-      return iterator(data + sz);
-    }
-  };
+  ConstVectorIterator<T> begin() const;
+  ConstVectorIterator<T> end() const;
+};
 
 }
 
+template <typename T>
+size_t smirnova::Vector<T>::size() const
+{
+  return sz;
+}
+
+template <typename T>
+T& smirnova::Vector<T>::operator[](size_t i)
+{
+  return data[i];
+}
+
+template <typename T>
+const T& smirnova::Vector<T>::operator[](size_t i) const
+{
+  return data[i];
+}
+
+template <typename T>
+void smirnova::Vector<T>::expansion(size_t newCap)
+{
+  if (newCap <= cap)
+    return;
+
+  T* newData = new T[newCap];
+
+  for (size_t i = 0; i < sz; ++i)
+    newData[i] = data[i];
+
+  delete[] data;
+  data = newData;
+  cap = newCap;
+}
+
+template <typename T>
+void smirnova::Vector<T>::ensureCapacity()
+{
+  if (sz < cap)
+    return;
+
+  size_t newCap = (cap == 0 ? 2 : cap * 2);
+  expansion(newCap);
+}
+
+template <typename T>
+void smirnova::Vector<T>::pushBack(const T& v)
+{
+  ensureCapacity();
+  data[sz++] = v;
+}
+
+template <typename T>
+void smirnova::Vector<T>::pushBack(T&& v)
+{
+  ensureCapacity();
+  data[sz++] = v;
+}
+
+template <typename T>
+smirnova::VectorIterator<T> smirnova::Vector<T>::begin()
+{
+  return VectorIterator<T>(data);
+}
+
+template <typename T>
+smirnova::VectorIterator<T> smirnova::Vector<T>::end()
+{
+  return VectorIterator<T>(data + sz);
+}
+
+template <typename T>
+smirnova::ConstVectorIterator<T> smirnova::Vector<T>::begin() const
+{
+  return ConstVectorIterator<T>(data);
+}
+
+template <typename T>
+smirnova::ConstVectorIterator<T> smirnova::Vector<T>::end() const
+{
+  return ConstVectorIterator<T>(data + sz);
+}
+
 #endif
+
