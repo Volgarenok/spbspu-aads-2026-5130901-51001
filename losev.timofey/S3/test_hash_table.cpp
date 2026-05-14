78119e054ef5b8c75b95a1927720620a6908a82a
L0syash <losev_timofey07@mail.ru>
2026-05-14 15:48:28 +0300

    Test: add failing HashTable tests (add/has/get)

--- losev.timofey/S3/test_hash_table.cpp
+++ losev.timofey/S3/test_hash_table.cpp
@@ -2,6 +2,7 @@
 #include <boost/test/included/unit_test.hpp>
 
 #include "sip_hash.hpp"
+#include "hash_table.hpp"
 
 BOOST_AUTO_TEST_SUITE(sip_hash_tests)
 
@@ -17,6 +18,24 @@ BOOST_AUTO_TEST_CASE(different_strings_different_hashes)
   uint64_t h1 = losev::hashString("hello");
   uint64_t h2 = losev::hashString("world");
   BOOST_CHECK_NE(h1, h2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(hash_table_tests)
+
+BOOST_AUTO_TEST_CASE(add_and_has)
+{
+  losev::HashTable<std::string, int> table(4);
+  table.add("key", 42);
+  BOOST_CHECK(table.has("key"));
+}
+
+BOOST_AUTO_TEST_CASE(get_returns_value)
+{
+  losev::HashTable<std::string, int> table(4);
+  table.add("key", 42);
+  BOOST_CHECK_EQUAL(table.get("key"), 42);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
