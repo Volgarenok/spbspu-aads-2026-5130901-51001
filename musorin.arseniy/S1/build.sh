13b8a57fe89b343e3a68b96ac1b10d47466376f9
arseniymusorin@gmail.com <quaka@quaka.localdomain>
2026-05-14 21:03:34 +0300

    fix 2 main (i got it)

--- musorin.arseniy/S1/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "$1" == "test" ]; then
-    echo "Building tests..."
-    g++ -std=c++17 -DTEST_MODE main.cpp test_list.cpp -o tests
-else
-    echo "Building main program..."
-    g++ -std=c++17 main.cpp test_list.cpp -o lab
-fi
