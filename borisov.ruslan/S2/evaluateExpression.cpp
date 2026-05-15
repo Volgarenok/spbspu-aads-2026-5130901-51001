a058aef69468b2ff8c3f2fc0dc19b51a762cced4
RBorisov6 <borisovruslan1512@gmail.com>
2026-05-15 22:33:41 +0300

    Implement tokenization with Queue

--- borisov.ruslan/S2/evaluateExpression.cpp
+++ borisov.ruslan/S2/evaluateExpression.cpp
@@ -1,5 +1,8 @@
 #include "evaluateExpression.hpp"
+#include "queue.hpp"
 #include <string>
+#include <sstream>
+#include <cstdlib>
 
 namespace borisov
 {
@@ -23,11 +26,62 @@ namespace borisov
       TokenType type;
       long long value = 0;
     };
+
+    void tokenize(const std::string& expr, Queue< Token >& tokens, std::string& errorMsg)
+    {
+      std::istringstream iss(expr);
+      std::string lex;
+      while (iss >> lex)
+      {
+        if (lex == "+")
+        {
+          tokens.push(Token{TokenType::op_plus});
+        }
+        else if (lex == "-")
+        {
+          tokens.push(Token{TokenType::op_minus});
+        }
+        else if (lex == "*")
+        {
+          tokens.push(Token{TokenType::op_mult});
+        }
+        else if (lex == "/")
+        {
+          tokens.push(Token{TokenType::op_div});
+        }
+        else if (lex == "%")
+        {
+          tokens.push(Token{TokenType::op_mod});
+        }
+        else if (lex == "!")
+        {
+          tokens.push(Token{TokenType::op_not});
+        }
+        else if (lex == "(")
+        {
+          tokens.push(Token{TokenType::lparen});
+        }
+        else if (lex == ")")
+        {
+          tokens.push(Token{TokenType::rparen});
+        }
+        else
+        {
+          char* end = nullptr;
+          long long val = std::strtoll(lex.c_str(), &end, 10);
+          if (*end != '\0')
+          {
+            errorMsg = "Invalid token: " + lex;
+            return;
+          }
+          tokens.push(Token{TokenType::number, val});
+        }
+      }
+    }
   }
 
   long long evaluateExpression(const std::string& expression, std::ostream& err)
   {
-    // будет реализовано позже
     return 0;
   }
 }
