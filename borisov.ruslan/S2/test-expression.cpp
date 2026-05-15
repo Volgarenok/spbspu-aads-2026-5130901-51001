db7a4949d5144e5853e3017551dba61f9dc9d7fe
RBorisov6 <borisovruslan1512@gmail.com>
2026-05-15 22:39:06 +0300

    Test infix to postfix conversion

--- borisov.ruslan/S2/test-expression.cpp
+++ borisov.ruslan/S2/test-expression.cpp
@@ -19,3 +19,29 @@ BOOST_AUTO_TEST_CASE(invalid_token_throws)
   BOOST_CHECK_THROW(borisov::evaluateExpression("x * 2", err), std::runtime_error);
   BOOST_CHECK_THROW(borisov::evaluateExpression("5 $ 3", err), std::runtime_error);
 }
+
+BOOST_AUTO_TEST_CASE(valid_infix_conversion_no_throw)
+{
+  std::ostringstream err;
+  BOOST_CHECK_NO_THROW(borisov::evaluateExpression("1 + 2", err));
+  BOOST_CHECK_NO_THROW(borisov::evaluateExpression("3 * 4 + 5", err));
+  BOOST_CHECK_NO_THROW(borisov::evaluateExpression("( 1 + 2 ) * 3", err));
+  BOOST_CHECK_NO_THROW(borisov::evaluateExpression("! 1", err));
+  BOOST_CHECK_NO_THROW(borisov::evaluateExpression("2 + ! 3", err));
+}
+
+BOOST_AUTO_TEST_CASE(mismatched_parentheses_throws)
+{
+  std::ostringstream err;
+  BOOST_CHECK_THROW(borisov::evaluateExpression("( 1 + 2", err), std::runtime_error);
+  BOOST_CHECK_THROW(borisov::evaluateExpression("1 + 2 )", err), std::runtime_error);
+  BOOST_CHECK_THROW(borisov::evaluateExpression(") 1 + 2 (", err), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(two_operands_in_row_throws)
+{
+  std::ostringstream err;
+  BOOST_CHECK_THROW(borisov::evaluateExpression("1 2 + 3", err), std::runtime_error);
+  BOOST_CHECK_THROW(borisov::evaluateExpression("+ 1 2", err), std::runtime_error);
+  BOOST_CHECK_THROW(borisov::evaluateExpression("1 + + 2", err), std::runtime_error);
+}
