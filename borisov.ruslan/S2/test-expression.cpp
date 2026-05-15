f2a16eb191ae179dc5b57231002b9329993c143b
RBorisov6 <borisovruslan1512@gmail.com>
2026-05-15 22:46:09 +0300

    Integrate parser into evaluateExpression

--- borisov.ruslan/S2/test-expression.cpp
+++ borisov.ruslan/S2/test-expression.cpp
@@ -45,3 +45,42 @@ BOOST_AUTO_TEST_CASE(two_operands_in_row_throws)
   BOOST_CHECK_THROW(borisov::evaluateExpression("+ 1 2", err), std::runtime_error);
   BOOST_CHECK_THROW(borisov::evaluateExpression("1 + + 2", err), std::runtime_error);
 }
+
+BOOST_AUTO_TEST_CASE(simple_evaluation)
+{
+  std::ostringstream err;
+  BOOST_CHECK_EQUAL(borisov::evaluateExpression("1 + 2", err), 3);
+  BOOST_CHECK_EQUAL(borisov::evaluateExpression("5 - 3", err), 2);
+  BOOST_CHECK_EQUAL(borisov::evaluateExpression("4 * 7", err), 28);
+  BOOST_CHECK_EQUAL(borisov::evaluateExpression("10 / 2", err), 5);
+  BOOST_CHECK_EQUAL(borisov::evaluateExpression("10 % 3", err), 1);
+}
+
+BOOST_AUTO_TEST_CASE(unary_not_evaluation)
+{
+  std::ostringstream err;
+  BOOST_CHECK_EQUAL(borisov::evaluateExpression("! 0", err), ~0LL);
+  BOOST_CHECK_EQUAL(borisov::evaluateExpression("! 1", err), ~1LL);
+  BOOST_CHECK_EQUAL(borisov::evaluateExpression("3 + ! 1", err), 3 + ~1LL);
+}
+
+BOOST_AUTO_TEST_CASE(precedence_evaluation)
+{
+  std::ostringstream err;
+  BOOST_CHECK_EQUAL(borisov::evaluateExpression("2 + 3 * 4", err), 14);
+  BOOST_CHECK_EQUAL(borisov::evaluateExpression("( 2 + 3 ) * 4", err), 20);
+  BOOST_CHECK_EQUAL(borisov::evaluateExpression("! 1 + 2", err), (~1LL) + 2);
+}
+
+BOOST_AUTO_TEST_CASE(division_by_zero_throws)
+{
+  std::ostringstream err;
+  BOOST_CHECK_THROW(borisov::evaluateExpression("1 / 0", err), std::runtime_error);
+  BOOST_CHECK_THROW(borisov::evaluateExpression("5 % 0", err), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(leftover_operands_throws)
+{
+  std::ostringstream err;
+  BOOST_CHECK_THROW(borisov::evaluateExpression("1 2", err), std::runtime_error);
+}
