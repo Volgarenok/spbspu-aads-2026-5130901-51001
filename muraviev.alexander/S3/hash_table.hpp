c300c7316f445d530052d65aee1b3eddbb084be4
a1dram <aldram54@gmail.com>
2026-05-16 23:34:04 +0300

    added HashTable normal lookup

--- muraviev.alexander/S3/hash_table.hpp
+++ muraviev.alexander/S3/hash_table.hpp
@@ -19,14 +19,15 @@ namespace muraviev
     };
 
     explicit HashTable(size_t slots):
-      buckets_(nullptr),
+      buckets_(new List< Entry >[slots]),
       bucketsCount_(slots),
-      size_(0)
+      size_(0),
+      hash_(),
+      equal_()
     {
       if (slots == 0) {
         throw std::invalid_argument("zero hash table size");
       }
-      buckets_ = new List< Entry >[slots];
     }
 
     ~HashTable()
@@ -34,20 +35,55 @@ namespace muraviev
       delete[] buckets_;
     }
 
-    size_t size() const
+    void add(const Key& key, const Value& value)
     {
-      return size_;
+      const size_t index = hash_(key) % bucketsCount_;
+      for (typename List< Entry >::iter it = buckets_[index].begin();
+          it != buckets_[index].end(); ++it) {
+        if (equal_(it->key, key)) {
+          it->value = value;
+          return;
+        }
+      }
+      Entry entry = {key, value};
+      if (buckets_[index].empty()) {
+        buckets_[index].pushFront(entry);
+      } else {
+        buckets_[index].insert(buckets_[index].last(), entry);
+      }
+      ++size_;
     }
 
-    bool empty() const
+    bool has(const Key& key) const
     {
-      return size_ == 0;
+      const size_t index = hash_(key) % bucketsCount_;
+      for (typename List< Entry >::c_iter it = buckets_[index].begin();
+          it != buckets_[index].end(); ++it) {
+        if (equal_(it->key, key)) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    Value& at(const Key& key)
+    {
+      const size_t index = hash_(key) % bucketsCount_;
+      for (typename List< Entry >::iter it = buckets_[index].begin();
+          it != buckets_[index].end(); ++it) {
+        if (equal_(it->key, key)) {
+          return it->value;
+        }
+      }
+      throw std::out_of_range("key not found");
     }
 
   private:
     List< Entry >* buckets_;
     size_t bucketsCount_;
     size_t size_;
+    Hash hash_;
+    Equal equal_;
   };
 }
 
