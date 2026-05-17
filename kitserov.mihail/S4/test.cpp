d83c3f778af787134f99e09f9806edead3290525
Kreniker <kitserov.mike@gmail.com>
2026-05-17 17:46:15 +0300

    start tests

---
+++ kitserov.mihail/S4/test.cpp
@@ -0,0 +1,12 @@
+#define BOOST_TEST_MODULE HashTests
+
+#include <utility>
+#include <boost/test/included/unit_test.hpp>
+#include "bitree.hpp"
+
+using namespace kitserov;
+
+BOOST_AUTO_TEST_CASE(bitree_start_empty)
+{
+
+}
