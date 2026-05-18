9fb59ec82ebc783ed580fc218fa46d489945bcb2
SmirnovaMarya <unlenmaria@gmail.com>
2026-05-18 23:08:08 +0300

    fixed tests

--- smirnova.marya/S3/test-hashTable.cpp
+++ smirnova.marya/S3/test-hashTable.cpp
@@ -1,4 +1,5 @@
-#include <boost/test/unit_test.hpp>
+#define BOOST_TEST_MODULE HashTableTests
+#include <boost/test/included/unit_test.hpp>
 
 #include "hashTable.hpp"
 #include <string>
@@ -10,100 +11,106 @@ BOOST_AUTO_TEST_SUITE(HashTableTests)
 BOOST_AUTO_TEST_CASE(DefaultConstruct)
 {
   HashTable<std::string, int> ht;
-  BOOST_TEST(ht.bucketCount() == 16);
-}
 
-BOOST_AUTO_TEST_CASE(AddAndHas)
-{
-  HashTable<std::string, int> ht;
-  BOOST_CHECK(!ht.has("key1"));
-
-  ht.add("key1", 42);
-  BOOST_CHECK(ht.has("key1"));
+  BOOST_CHECK_EQUAL(ht.bucketCount(), 16);
+  BOOST_CHECK_EQUAL(ht.size(), 0);
 }
 
 BOOST_AUTO_TEST_CASE(AddAndGet)
 {
   HashTable<std::string, int> ht;
-  ht.add("key1", 100);
 
-  BOOST_CHECK_EQUAL(ht.get("key1"), 100);
-}
+  BOOST_CHECK(!ht.has("key1"));
 
-BOOST_AUTO_TEST_CASE(AddMultiple)
-{
-  HashTable<std::string, int> ht;
-  ht.add("a", 1);
-  ht.add("b", 2);
-  ht.add("c", 3);
+  ht.add("key1", 42);
 
-  BOOST_CHECK_EQUAL(ht.get("a"), 1);
-  BOOST_CHECK_EQUAL(ht.get("b"), 2);
-  BOOST_CHECK_EQUAL(ht.get("c"), 3);
+  BOOST_CHECK(ht.has("key1"));
+  BOOST_CHECK_EQUAL(ht.get("key1"), 42);
+  BOOST_CHECK_EQUAL(ht.size(), 1);
 }
 
 BOOST_AUTO_TEST_CASE(UpdateValue)
 {
   HashTable<std::string, int> ht;
-  ht.add("key", 10);
-  BOOST_CHECK_EQUAL(ht.get("key"), 10);
 
-  ht.add("key", 20);
-  BOOST_CHECK_EQUAL(ht.get("key"), 20);
+  ht.add("key1", 10);
+  BOOST_CHECK_EQUAL(ht.get("key1"), 10);
+
+  ht.add("key1", 20);
+
+  BOOST_CHECK_EQUAL(ht.get("key1"), 20);
+  BOOST_CHECK_EQUAL(ht.size(), 1);
+}
+
+BOOST_AUTO_TEST_CASE(AddMultiple)
+{
+  HashTable<std::string, int> ht;
+
+  ht.add("key1", 1);
+  ht.add("key2", 2);
+  ht.add("key3", 3);
+
+  BOOST_CHECK_EQUAL(ht.get("key1"), 1);
+  BOOST_CHECK_EQUAL(ht.get("key2"), 2);
+  BOOST_CHECK_EQUAL(ht.get("key3"), 3);
+
+  BOOST_CHECK_EQUAL(ht.size(), 3);
 }
 
 BOOST_AUTO_TEST_CASE(Drop)
 {
   HashTable<std::string, int> ht;
+
   ht.add("key1", 50);
+  ht.add("key2", 60);
+  ht.add("key3", 70);
 
-  int dropped = ht.drop("key1");
-  BOOST_CHECK_EQUAL(dropped, 50);
+  BOOST_CHECK_EQUAL(ht.drop("key1"), 50);
+
   BOOST_CHECK(!ht.has("key1"));
-}
+  BOOST_CHECK(ht.has("key2"));
+  BOOST_CHECK(ht.has("key3"));
 
-BOOST_AUTO_TEST_CASE(DropThrowsOnNotFound)
-{
-  HashTable<std::string, int> ht;
-
-  BOOST_CHECK_THROW(ht.drop("nonexistent"), std::runtime_error);
-}
-
-BOOST_AUTO_TEST_CASE(GetThrowsOnNotFound)
-{
-  HashTable<std::string, int> ht;
-
-  BOOST_CHECK_THROW(ht.get("nonexistent"), std::runtime_error);
-}
-
-BOOST_AUTO_TEST_CASE(BucketCount)
-{
-  HashTable<std::string, int> ht;
-  BOOST_CHECK_EQUAL(ht.bucketCount(), 16);
-}
-
-BOOST_AUTO_TEST_CASE(Rehash)
-{
-  HashTable<std::string, int> ht;
-  ht.add("key1", 1);
-  ht.add("key2", 2);
-
-  ht.rehash(32);
-
-  BOOST_CHECK_EQUAL(ht.bucketCount(), 32);
-  BOOST_CHECK_EQUAL(ht.get("key1"), 1);
-  BOOST_CHECK_EQUAL(ht.get("key2"), 2);
+  BOOST_CHECK_EQUAL(ht.size(), 2);
 }
 
 BOOST_AUTO_TEST_CASE(HasReturnsFalseForMissing)
 {
   HashTable<std::string, int> ht;
-  ht.add("exists", 1);
 
-  BOOST_CHECK(ht.has("exists"));
+  ht.add("key1", 1);
+  ht.add("key2", 2);
+  ht.add("key3", 3);
+
+  BOOST_CHECK(ht.has("key1"));
+  BOOST_CHECK(ht.has("key2"));
+  BOOST_CHECK(ht.has("key3"));
+
   BOOST_CHECK(!ht.has("missing"));
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_CASE(Rehash)
+{
+  HashTable<std::string, int> ht;
 
+  ht.add("key1", 1);
+  ht.add("key2", 2);
+  ht.add("key3", 3);
+
+  ht.rehash(32);
+
+  BOOST_CHECK_EQUAL(ht.bucketCount(), 32);
+
+  BOOST_CHECK(ht.has("key1"));
+  BOOST_CHECK(ht.has("key2"));
+  BOOST_CHECK(ht.has("key3"));
+
+  BOOST_CHECK_EQUAL(ht.get("key1"), 1);
+  BOOST_CHECK_EQUAL(ht.get("key2"), 2);
+  BOOST_CHECK_EQUAL(ht.get("key3"), 3);
+
+  BOOST_CHECK_EQUAL(ht.size(), 3);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
 
