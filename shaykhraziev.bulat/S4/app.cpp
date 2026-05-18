9dcb899819149dc83d2e91b7b9fb9b45edb1086c
CWSHBR <mrcookie209@gmail.com>
2026-05-18 14:05:13 +0300

    Wire S4 app and main

--- shaykhraziev.bulat/S4/app.cpp
+++ shaykhraziev.bulat/S4/app.cpp
@@ -1,9 +1,32 @@
 #include "app.hpp"
 
-#include <iostream>
+#include "commands.hpp"
+#include "io.hpp"
 
-int shaykhraziev::runS4(const char*, std::istream&, std::ostream&, std::ostream& err)
+#include <iostream>
+#include <stdexcept>
+
+int shaykhraziev::runS4(const char* filename, std::istream& in, std::ostream& out, std::ostream& err)
 {
-  err << "S4 is not implemented\n";
-  return 1;
+  try
+  {
+    DatasetTable datasets = readDatasetsFromFile(filename);
+    processCommands(datasets, in, out);
+  }
+  catch (const std::runtime_error& e)
+  {
+    err << e.what() << '\n';
+    return 1;
+  }
+  catch (const std::logic_error& e)
+  {
+    err << e.what() << '\n';
+    return 1;
+  }
+  catch (const std::exception& e)
+  {
+    err << e.what() << '\n';
+    return 2;
+  }
+  return 0;
 }
