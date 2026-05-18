bd16285aa1ec701af56c636c4cc7125bf5bef9d0
CWSHBR <mrcookie209@gmail.com>
2026-05-18 14:02:51 +0300

    Add S4 input parsing

--- shaykhraziev.bulat/S4/io.hpp
+++ shaykhraziev.bulat/S4/io.hpp
@@ -1,7 +1,29 @@
 #ifndef S4_IO_HPP
 #define S4_IO_HPP
 
+#include <bstree.hpp>
+
+#include <iosfwd>
+#include <string>
+
 namespace shaykhraziev
-{}
+{
+  struct IntLess
+  {
+    bool operator()(int lhs, int rhs) const;
+  };
+
+  struct StringLess
+  {
+    bool operator()(const std::string& lhs, const std::string& rhs) const;
+  };
+
+  using Dictionary = BSTree< int, std::string, IntLess >;
+  using DatasetTable = BSTree< std::string, Dictionary, StringLess >;
+
+  bool parseInt(const std::string& value, int& result);
+  DatasetTable readDatasets(std::istream& in);
+  DatasetTable readDatasetsFromFile(const char* filename);
+}
 
 #endif
---
+++ shaykhraziev.bulat/S4/test-io.cpp
@@ -0,0 +1,59 @@
+#include <boost/test/unit_test.hpp>
+
+#include "io.hpp"
+
+#include <sstream>
+#include <stdexcept>
+
+BOOST_AUTO_TEST_CASE(io_reads_valid_datasets)
+{
+  std::istringstream input("first 1 name 2 surname\nsecond 4 mouse 1 keyboard\n");
+
+  shaykhraziev::DatasetTable datasets = shaykhraziev::readDatasets(input);
+
+  BOOST_CHECK(datasets.has("first"));
+  BOOST_CHECK(datasets.has("second"));
+  BOOST_TEST(datasets.get("first").get(1) == "name");
+  BOOST_TEST(datasets.get("first").get(2) == "surname");
+  BOOST_TEST(datasets.get("second").get(4) == "mouse");
+}
+
+BOOST_AUTO_TEST_CASE(io_ignores_empty_lines)
+{
+  std::istringstream input("\nfirst 1 one\n\nsecond\n");
+
+  shaykhraziev::DatasetTable datasets = shaykhraziev::readDatasets(input);
+
+  BOOST_CHECK(datasets.has("first"));
+  BOOST_CHECK(datasets.has("second"));
+  BOOST_CHECK(datasets.get("second").empty());
+}
+
+BOOST_AUTO_TEST_CASE(io_reads_empty_dictionary)
+{
+  std::istringstream input("empty\n");
+
+  shaykhraziev::DatasetTable datasets = shaykhraziev::readDatasets(input);
+
+  BOOST_CHECK(datasets.has("empty"));
+  BOOST_CHECK(datasets.get("empty").empty());
+}
+
+BOOST_AUTO_TEST_CASE(io_rejects_odd_key_value_tail)
+{
+  std::istringstream input("bad 1 one 2\n");
+
+  BOOST_CHECK_THROW(shaykhraziev::readDatasets(input), std::logic_error);
+}
+
+BOOST_AUTO_TEST_CASE(io_rejects_invalid_key)
+{
+  std::istringstream input("bad x value\n");
+
+  BOOST_CHECK_THROW(shaykhraziev::readDatasets(input), std::logic_error);
+}
+
+BOOST_AUTO_TEST_CASE(io_rejects_missing_file)
+{
+  BOOST_CHECK_THROW(shaykhraziev::readDatasetsFromFile("__missing_s4_file__"), std::runtime_error);
+}
