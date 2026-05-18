2c9f3c26ac514c36e7cc966c3af6230a2b384e86
ahahhaha67 <krivoshapovewan@yandex.ru>
2026-05-18 21:38:50 +0300

    feat(main): entry point read

--- krivoshapov.ivan/S3/main.cpp
+++ krivoshapov.ivan/S3/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <fstream>
 
-int main()
+int main(int argc, char **argv)
 {
-  std::cout << "Hello, S3!" << std::endl;
+  if (argc < 2)
+  {
+    std::cerr << "ERROR: filename argument required\n";
+    return 1;
+  }
+  std::ifstream file(argv[1]);
+  if (!file.is_open())
+  {
+    std::cerr << "ERROR: cannot open file\n";
+    return 1;
+  }
   return 0;
 }
