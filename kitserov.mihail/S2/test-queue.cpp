c13a962eaf1f7d3b96a2482f0486f0394ebcc213
Kreniker <kitserov.mike@gmail.com>
2026-05-19 22:34:40 +0300

    test -> test-main and test-queue

--- kitserov.mihail/S2/test-queue.cpp
+++ kitserov.mihail/S2/test-queue.cpp
@@ -1,6 +1,5 @@
-#define BOOST_TEST_MODULE QueueStackTests
+#include <boost/test/unit_test.hpp>
 #include <stdexcept>
-#include <boost/test/included/unit_test.hpp>
 #include "queue.hpp"
 #include "stack.hpp"
 
